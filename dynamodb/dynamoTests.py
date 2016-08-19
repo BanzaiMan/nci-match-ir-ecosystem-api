59bf3c1fadd86d0a4ca7dbb3696c8d4d11d15b78
qqss88 <qqss88@gmail.com>
2016-08-19 15:36:08 -0400

    adding tst_load.json

--- dynamodb/dynamoTests.py
+++ dynamodb/dynamoTests.py
@@ -1,7 +1,21 @@
 import aws
 from aws import DynamoDBService
-from boto3.dynamodb.conditions import Key, Attr
+import boto3
 import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+import sys
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
 
 def test_local():
     local_conn = DynamoDBService.get_local_connection()
@@ -21,19 +35,19 @@ def test_production():
 
 def create_table(table_name):
     db_service = DynamoDBService()
-    dynamo_db = db_service.get_db_connection()
+    dynamo_db = db_service.get_local_connection()
     table = dynamo_db.create_table(
         TableName=table_name,
         KeySchema=[
-        { 'AttributeName': '_id',
+        { 'AttributeName': 'site',
           'KeyType': 'HASH'},
-        { 'AttributeName': '_class',
+        { 'AttributeName': 'date_molecular_id_created',
           'KeyType': 'RANGE' }
     ],
         AttributeDefinitions=[
-        { 'AttributeName': '_id',
+        { 'AttributeName': 'site',
           'AttributeType': 'S' },
-        { 'AttributeName': '_class',
+        { 'AttributeName': 'date_molecular_id_created',
           'AttributeType': 'S' }
     ],
         ProvisionedThroughput={
@@ -43,63 +57,100 @@ def create_table(table_name):
     )
     print('Table status:', table.table_status)
 
-def load_item(json_file):
+def load_items(json_file, table_name):
     db_service = DynamoDBService()
-    dynamo_db = db_service.get_db_connection()
-    control_table = dynamo_db.Table('sampleControl')
+    dynamo_db = db_service.get_local_connection()
+    control_table = dynamo_db.Table(table_name)
 
     with open(json_file) as json_f:
-        sample_ctrl = json.load(json_f)
-        _id = sample_ctrl['_id']
-        _class = sample_ctrl['_class']
-        molecularSequenceNumber = sample_ctrl['molecularSequenceNumber']
-        dateCreated = sample_ctrl['dateCreated']
-        dateReceived = sample_ctrl['dateReceived']
-        site = sample_ctrl['site']
-        siteIpAddress = sample_ctrl['siteIpAddress']
-        positiveControlVersion = int(sample_ctrl['positiveControlVersion'])
-        positiveControlDateLoaded = sample_ctrl['positiveControlDateLoaded']
-        status = sample_ctrl['status']
-        passed = bool(sample_ctrl['passed'])
-        nextGenerationSequence = sample_ctrl['nextGenerationSequence']
+        sample_ctrls = json.load(json_f)
+        items = sample_ctrls['Items']
+        for item in items:
+        # _id = sample_ctrl['_id']
+        # _class = sample_ctrl['_class']
+        # molecularSequenceNumber = sample_ctrl['molecularSequenceNumber']
+        # dateCreated = sample_ctrl['dateCreated']
+        # dateReceived = sample_ctrl['dateReceived']
+        # site = sample_ctrl['site']
+        # siteIpAddress = sample_ctrl['siteIpAddress']
+        # positiveControlVersion = int(sample_ctrl['positiveControlVersion'])
+        # positiveControlDateLoaded = sample_ctrl['positiveControlDateLoaded']
+        # status = sample_ctrl['status']
+        # passed = bool(sample_ctrl['passed'])
+        # nextGenerationSequence = sample_ctrl['nextGenerationSequence']
 
-        print 'adding sample control:'+molecularSequenceNumber
+            print 'adding sample control:', item["molecular_id"], item['type']
 
-        control_table.put_item(
-            Item={
-                '_id': _id,
-                '_class': _class,
-                'molecularSequenceNumber': molecularSequenceNumber,
-                'dateCreated': dateCreated,
-                'dateReceived': dateReceived,
-                'site': site,
-                'siteIpAddress': siteIpAddress,
-                'positiveControlVersion': positiveControlVersion,
-                'positiveControlDateLoaded': positiveControlDateLoaded,
-                'status': status,
-                'passed': passed,
-                'nextGenerationSequence': nextGenerationSequence
-            }
+            control_table.put_item(
+                Item={
+                    "date_molecular_id_created": item["date_molecular_id_created"],
+                    "type": item["type"],
+                    "site_ip_address": item["site_ip_address"],
+                    "molecular_id": item["molecular_id"],
+                    "site": item["site"]
+                }
         )
 
-
-def validate_item(msn):
+def query_items(table_name):
     db_service = DynamoDBService()
-    dynamo_db = db_service.get_db_connection()
-    control_table = dynamo_db.Table('sampleControl')
-    response = control_table.query(
-        KeyConditionExpression=Key('_id').eq('_'.join(msn.split('_')[1:]))
+    dynamo_db = db_service.get_local_connection()
+    table = dynamo_db.Table(table_name)
+
+    query_str = '{"site":"MoCha","type":"positive"}'
+    print "filter_condition: Attr('site').eq('MoCha')&Attr('type').eq('positive')"
+    attrs = json.loads(query_str)
+    fe = None
+    for attr in attrs:
+        if fe == None:
+            fe = Attr(attr).eq(attrs[attr])
+        else:
+            fe = fe & Attr(attr).eq(attrs[attr])
+
+    response = table.scan(
+        FilterExpression=fe,
     )
-    return response['Items'] > 0
+
+    for i in response['Items']:
+        print(json.dumps(i, cls=DecimalEncoder))
+
+    while 'LastEvaluatedKey' in response:
+        response = table.scan(
+            ProjectionExpression=pe,
+        )
+
+        for i in response['Items']:
+            print(json.dumps(i, cls=DecimalEncoder))
+
+
+
+
+
+# def validate_item(msn):
+#     db_service = DynamoDBService()
+#     dynamo_db = db_service.get_db_connection()
+#     control_table = dynamo_db.Table('sampleControl')
+#     response = control_table.query(
+#         KeyConditionExpression=Key('_id').eq('_'.join(msn.split('_')[1:]))
+#     )
+#     return response['Items'] > 0
 
 
 
 
 if __name__ == '__main__':
+    table_name = sys.argv[1]
+    #query_items(table_name)
 
     #test_production()
-    #create_table('sampleControl')
-    #load_item('/Users/sunq3/match_data/sampleControl/s2.txt')
-    print validate_item('SampleControl_MDACC_1')
+    print 'creating table:'+ table_name
+    create_table(table_name)
+
+    print 'loading some items:'
+    load_items('tst_load.json', table_name)
+
+    print 'query demo:'
+    query_items(table_name)
+
+    #print validate_item('SampleControl_MDACC_1')
 
 
