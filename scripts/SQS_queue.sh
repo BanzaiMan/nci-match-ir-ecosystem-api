12c6f71be23af9efa39a7caed0cdf6e75b1d3885
wosman <waleed.a.osman@gmail.com>
2016-08-22 13:45:55 -0400

    Added send message sample script and sample send message json.

--- scripts/SQS_queue.sh
+++ scripts/SQS_queue.sh
@@ -9,7 +9,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 QUEUE_NAME="update_queue"
-DATA_FILE="file://sqs_message.json"
+DATA_FILE="file://send_message.json"
 
 END_POINT=""
 
@@ -19,7 +19,7 @@ echo -e "${CYAN}***********************************************${NC}"
 echo -e "${RED}DELETES QUEUE IF IT EXISTS${NC}"
 echo -e "${CYAN}***********************************************${NC}"
 
-aws sqs delete-queue --queue-url https://sqs.us-west-2.amazonaws.com/127516845550/update_queue
+# aws sqs delete-queue --queue-url https://sqs.us-west-2.amazonaws.com/127516845550/update_queue
 
 echo -e "${CYAN}***********************************************${NC}"
 echo -e "${RED}CREATES QUEUE WITH DEFAULT ATTRIBUTES${NC}"
@@ -38,3 +38,9 @@ echo -e "${RED}LISTS QUEUES${NC}"
 echo -e "${CYAN}***********************************************${NC}"
 
 aws sqs list-queues
+
+echo -e "${CYAN}***********************************************${NC}"
+echo -e "${RED}SENDS MESSAGE${NC}"
+echo -e "${CYAN}***********************************************${NC}"
+
+aws sqs send-message --queue-url https://sqs.us-west-2.amazonaws.com/127516845550/update_queue --message-body "A Sample from MoCha, #3." --delay-seconds 10 --message-attributes file://send_message.json
