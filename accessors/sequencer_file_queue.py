cd10929534f4cd1fe8b3c5865d916b8da2021c7c
wosman <waleed.a.osman@gmail.com>
2016-08-21 20:32:43 -0400

    Renamed queue to 'WHAT it contains'

--- accessors/sequencer_file_queue.py
+++ accessors/sequencer_file_queue.py
@@ -2,10 +2,10 @@ from aws_sqs_accessor import AwsSqsAccessor
 
 
 class SequencerFileQueue(AwsSqsAccessor):
-        # TODO: Rename queue to 'WHAT it contains' instead of naming it to'WHEN it is used' as we may change the WHEN
         def __init__(self):
-            super(SequencerFileQueue, self).__init__('after_s3_upload')
+            super(SequencerFileQueue, self).__init__('files_from_ir_sequencer')
 
         # TODO: What if it fails to write, like the queue is down? Should catch errors and return errors.
         def write(self, message):
+            print 'Hello'
             self.queue.send_message(MessageBody=message)
