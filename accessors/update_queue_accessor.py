f6ae9f3044881ab9fd9b06781bfaaeaf6f14fb91
wosman <waleed.a.osman@gmail.com>
2016-08-24 09:07:56 -0400

    Completed TODO

--- accessors/update_queue_accessor.py
+++ accessors/update_queue_accessor.py
@@ -6,8 +6,6 @@ class UpdateQueueAccessor(SqsAccessor):
         def __init__(self):
             SqsAccessor.__init__(self, 'update_queue','us-east-1')
 
-        # TODO: Add logging
-        # TODO: What if it fails to write, like the queue is down? Should catch errors and return errors.
         def write(self, message):
             self.logger.info('Attempting to POST new message to queue')
             try:
