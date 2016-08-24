e61c8be1916bb220957fca4a49a8b510a0c7682c
bcoffe <brent.coffey@nih.gov>
2016-08-24 07:22:03 -0400

    Fixed file size generation

--- scripts/database/sample_control_generator.py
+++ scripts/database/sample_control_generator.py
@@ -11,10 +11,6 @@ sys.path.insert(1, path)
 from common.string_helper import StringHelper
 from common.datetime_helper import DateTimeHelper
 
-
-HEAD = '{"sample_controls": ['
-TAIL = ']}'
-
 item_template = '''
     {
       "PutRequest": {
@@ -49,20 +45,22 @@ def generate_by_number(number=10):
 
 def write_to_file(items, file_name):
     with open(file_name, 'w') as f_out:
-        pretty_json = json.loads(HEAD + items + TAIL)
-        f_out.write(json.dumps(pretty_json, indent=4, sort_keys=True))
-
-    print 'generation done.'
+        pretty_json = json.loads('{"sample_controls": [' + items + ']}')
+        f_out.write(json.dumps(pretty_json,  indent=4, sort_keys=True))
 
 
 def generate_by_size(file_name, size=1000):
-    pass
-    # with open(file_name, 'w') as f_out:
-    #     f_out.write(compose_a_message())
-    #     while os.path.getsize(file_name) < size:
-    #         f_out.write(compose_a_message())
-    # print 'done.'
-    # return
+    print 'generating file that is a little greater than ' + str(size) + ' megabyte(s).'
+    file_too_small = True
+    items = ""
+    while file_too_small:
+        items += (','.join(item_template % (build_an_item()) for _ in range(5)))
+        write_to_file(items, file_name)
+        if os.path.getsize(file_name) > size*1000000:
+            file_too_small = False
+        else:
+            print "File size: " + str(os.path.getsize(file_name)) + " continuing to build..."
+            items += ","
 
 
 def print_usage():
@@ -73,16 +71,18 @@ def main(argv):
     parser = argparse.ArgumentParser(add_help=True)
     parser.add_argument("-o", type=str, required=True, dest="json_file_output",
                         help="output file. Required.")
-    parser.add_argument("-n", type=str, required=True, dest="number", help="sample control items generated. Required.")
-    # parser.add_argument("-s", type=str, required=False, dest="file_size", help="file_size generated in byte. Optional.")
+    parser.add_argument("-n", type=str, required=False, dest="number",
+                        help="sample control items generated. Optional.")
+    parser.add_argument("-s", type=str, required=False, dest="file_size",
+                        help="file_size generated in megabytes. Optional.")
 
     args = parser.parse_args(argv)
-    # print args
 
-    # if not ars.number is None:
-    write_to_file(generate_by_number(int(args.number)), args.json_file_output)
-    # else:
-    #    generate_by_size(args.json_file_output, args.file_size)
+    print sys.getsizeof('')
+    if args.number is not None:
+        write_to_file(generate_by_number(int(args.number)), args.json_file_output)
+    else:
+        generate_by_size(args.json_file_output, int(args.file_size))
 
 if __name__ == '__main__':
     main(sys.argv[1:])
