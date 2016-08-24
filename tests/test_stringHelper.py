47678e8155ef51d2526f14e8599f65a04664756e
wosman <waleed.a.osman@gmail.com>
2016-08-24 16:29:03 -0400

    Altered unit tests to follow SOLID OOD principles.

--- tests/test_stringHelper.py
+++ tests/test_stringHelper.py
@@ -1,37 +1,27 @@
-from unittest import TestCase
 import unittest
+from mock import patch, MagicMock
 import string
-import logging
-import sys
-sys.path.append('..')
+import os
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 from common.string_helper import StringHelper
 from ddt import ddt, data, unpack
 
 @ddt
-class TestStringHelper(TestCase):
-    def test_generate_molecular_id(self):
-        logger = logging.getLogger(__name__)
-        logger.info("Testing if random string contains 'SC_' in front")
+class TestStringHelper(unittest.TestCase):
+
+    @patch('common.string_helper.StringHelper.generate_random_string')
+    def test_generate_molecular_id(self, random_string_function):
+        random_string_function.return_value = "TR45G"
+        print "Testing if random string contains 'SC_' in front"
         random_string = StringHelper.generate_molecular_id(5)
         assert random_string.startswith("SC_")
 
-    @data((5,0,8))
+    @data((5,5),(0,0),(8,8))
     @unpack
-    def test_generate_random_string(self, len1, len2, len3):
-        logger = logging.getLogger(__name__)
-        logger.info("Testing if random string length equals 5 and if it contains uppercase letters or numbers")
-
-        random_string = StringHelper.generate_random_string()
-        assert len(random_string) == len1
-
-        random_string = StringHelper.generate_random_string(len1)
-        assert len(random_string) == len1
-
-        random_string = StringHelper.generate_random_string(len2)
-        assert random_string == ''
-
-        random_string = StringHelper.generate_random_string(len3)
-        assert not len(random_string) == len1
+    def test_generate_random_string(self, random_length, expected_length):
+        print "Testing if random string and if it contains uppercase letters or numbers"
+        random_string = StringHelper.generate_random_string(random_length)
+        assert len(random_string) == expected_length
 
         for m in random_string:
             assert (m in string.ascii_uppercase or m in string.digits)
