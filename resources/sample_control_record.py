c307fb28228c320e089f6139dd47c5dd2efffb6f
bcoffe <brent.coffey@nih.gov>
2016-08-25 07:16:27 -0400

    Improved unittests
    - Part of unit testing is passing improper arguments to the method
    being tested and ensuring the method handles invalid input
    appropriately. This unit test showed that the code did not originally
    handle those conditions properly, so the code had to be changed. This
    is a good example of how unites improve your code and force one to
    think of things that had not originally considered.

--- resources/sample_control_record.py
+++ resources/sample_control_record.py
@@ -2,18 +2,22 @@ import logging
 from accessors.sample_control_accessor import SampleControlAccessor
 from flask_restful import abort, Resource
 
+# TODO: Qing, in order to take in query parameters you need to put code here to recieve query parameters. Look at the example in the sample_control_table.py
 
 class SampleControlRecord(Resource):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
-    #def put(self, molecular_id):
-    def put(self, molecular_id, update_expression, expression_attribute_values):
+    def put(self, molecular_id):
+        # TODO: Qing, the caller will not know what the update_expresion, or expression_attribute_values will be. Nor should they because they shouldn't know we are using dynamodb.
+        # you need methods that take a dictionary and creates these values. This is what the common.Query Helper class
+        # is for.  Recall that the person calling this particular method is just putting in a
+        # def put(self, molecular_id, update_expression, expression_attribute_values):
         self.logger.info("updating sample control with id: " + str(molecular_id))
         try:
             # TODO: Instead of writing directly put on queue and then pop off queue to do delete
-            SampleControlAccessor().update_item({'molecular_id': molecular_id}, update_expression,
-                                                expression_attribute_values)
+            # SampleControlAccessor().update_item({'molecular_id': molecular_id}, update_expression,
+            #                                   expression_attribute_values)
             return {"message": "Item updated", "molecular_id": molecular_id}
         except Exception, e:
             self.logger.debug("updated_item failed because" + e.message)
--- tests/test_stringHelper.py
+++ tests/test_stringHelper.py
@@ -1,10 +1,11 @@
 import unittest
 from mock import patch, MagicMock
 import string
-import os
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+import sys
+sys.path.append("..")
 from common.string_helper import StringHelper
 from ddt import ddt, data, unpack
+
 
 @ddt
 class TestStringHelper(unittest.TestCase):
@@ -16,15 +17,17 @@ class TestStringHelper(unittest.TestCase):
         random_string = StringHelper.generate_molecular_id(5)
         assert random_string.startswith("SC_")
 
-    @data((5,5),(0,0),(8,8))
+    @data((5, 5, ""), (0, "", "Invalid Input"), (8, 8, ""), (-1, "", "Invalid Input"), ("dog", "", "Invalid Input"))
     @unpack
-    def test_generate_random_string(self, random_length, expected_length):
+    def test_generate_random_string(self, random_length, expected_length, exception_message):
         print "Testing if random string and if it contains uppercase letters or numbers"
-        random_string = StringHelper.generate_random_string(random_length)
-        assert len(random_string) == expected_length
-
-        for m in random_string:
-            assert (m in string.ascii_uppercase or m in string.digits)
+        try:
+            random_string = StringHelper.generate_random_string(random_length)
+            assert len(random_string) == expected_length
+            for m in random_string:
+                assert (m in string.ascii_uppercase or m in string.digits)
+        except ValueError, e:
+            assert (e.message == exception_message)
 
 if __name__ == '__main__':
     unittest.main()
