161cd5d3671fe5c77797ad77a9e6a6e93c9ca6e3
wosman <waleed.a.osman@gmail.com>
2016-08-25 08:30:03 -0400

    Added test for floating point and removed print statements.

--- tests/test_stringHelper.py
+++ tests/test_stringHelper.py
@@ -13,14 +13,12 @@ class TestStringHelper(unittest.TestCase):
     @patch('common.string_helper.StringHelper.generate_random_string')
     def test_generate_molecular_id(self, random_string_function):
         random_string_function.return_value = "TR45G"
-        print "Testing if random string contains 'SC_' in front"
         random_string = StringHelper.generate_molecular_id(5)
         assert random_string.startswith("SC_")
 
-    @data((5, 5, ""), (0, "", "Invalid Input"), (8, 8, ""), (-1, "", "Invalid Input"), ("dog", "", "Invalid Input"))
+    @data((5, 5, ""), (0, "", "Invalid Input"), (8, 8, ""), (-1, "", "Invalid Input"), ("dog", "", "Invalid Input"), (5.032, "", "Invalid Input"))
     @unpack
     def test_generate_random_string(self, random_length, expected_length, exception_message):
-        print "Testing if random string and if it contains uppercase letters or numbers"
         try:
             random_string = StringHelper.generate_random_string(random_length)
             assert len(random_string) == expected_length
