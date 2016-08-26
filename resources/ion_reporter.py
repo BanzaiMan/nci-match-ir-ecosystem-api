53dd3d9f2c974564bd46c9f79565a5b0daf43b90
wosman <waleed.a.osman@gmail.com>
2016-08-26 17:25:53 -0400

    added celery functionality

--- resources/ion_reporter.py
+++ resources/ion_reporter.py
@@ -1,14 +1,25 @@
 import json
 import logging
-from flask_restful import abort, request, Resource
+from flask_restful import abort, request, reqparse, Resource
 from accessors.update_queue_accessor import UpdateQueueAccessor
 from common.schemas import Schemas
 from jsonschema import validate, ValidationError
+from accessors.celery_task_accessor import CeleryTaskAccessor
 
+parser = reqparse.RequestParser()
+
+# TODO: lower priority, but could expand to consider more of the attributes
+parser.add_argument('analysis_id', type=str, required=False)
+parser.add_argument('site',         type=str, required=False)
+parser.add_argument('dna_bam_file_name', type=str, required=False)
+parser.add_argument('patient_id', type=str, required=False)
+parser.add_argument('cdna_bam_file_name', type=str, required=False)
+parser.add_argument('vcf_file_name', type=str, required=False)
+parser.add_argument('molecular_id', type=str, required=False)
 
 # curl -X POST -H "Content-Type: application/json" -d '{ "molecular_id":"123", "analysis_id":"fork",
 # "patient_id":"fork", "site":"fork", "vcf_file_name":"fork", "dna_bam_file_name":"fork", "cdna_bam_file_name":"fork"}'
-# "http://localhost:5000/ion_reporters"
+# "http://localhost:5000/ion_reporters/SC_SCA14"
 class IonReporter(Resource):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
@@ -27,7 +38,8 @@ class IonReporter(Resource):
         self.logger.info('JSON validated')
         try:
             # TODO:Change to use celery
-            UpdateQueueAccessor().write(json.dumps(input_json))
+            #UpdateQueueAccessor().write(json.dumps(input_json))
+            CeleryTaskAccessor().message_body_item(json.dumps(input_json))
             self.logger.info('New Ion Reporter file names POSTED to SQS queue')
         except Exception, e:
             self.logger.error("Unable to import message details: " + e.message)
