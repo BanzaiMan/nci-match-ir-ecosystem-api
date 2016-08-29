0855a9604c0df35c02e9906f9b4df8420d72dcbf
yuq4 <qing.yu2@nih.gov>
2016-08-29 17:23:09 -0400

    add s3_uploader

--- accessors/s3_accessor.py
+++ accessors/s3_accessor.py
@@ -36,9 +36,7 @@ def s3_downloadable(file_extension):
 
 class S3Accessor(object):
 
-    ## TODO make S3 connection without using .s3_config.json configure file
     def __init__(self):
-        self.__access_key, self.__secret_key = aws_credentials()
         self.logger = logging.getLogger(__name__)
         self.logger.debug("S3Accessor instantiated")
 
@@ -55,49 +53,52 @@ class S3Accessor(object):
             config = json.load(config_file)
             return config
 
-    def get_s3_connection(self):
+    def get_s3_resource(self):
         try:
-            s3 = boto3.resource('s3', aws_access_key_id=self.__access_key, aws_secret_access_key=self.__secret_key)
-        except:
-            print 'S3 connection cannot be established. Please double check your credentials and network connection.'
+            s3 = boto3.resource('s3')
+            self.logger.info("Checking database for sample_control table existence.")
+            print "S3 resource connected."
+        except Exception, e:
+            self.logger.debug("S3 resource connection failed because: " + e.message)
+            print 'S3 resource connection cannot be established. ' + str(e.message)
             return None
         return s3
 
-    def get_s3_transfer(self):
-        # multipart_threshold=9999999999999999,  # workaround for 'disable' auto multipart upload
-        my_config = TransferConfig(
-            multipart_threshold=8000000,
-            max_concurrency=10,
-            num_download_attempts=10,
-        )
-
-        connection = boto3.client(service_name='s3',
-                                  region_name='us-east-1',
-                                  api_version=None,
-                                  use_ssl=True,
-                                  verify=True,
-                                  aws_access_key_id=self.__access_key,
-                                  aws_secret_access_key=self.__secret_key,
-                                  aws_session_token=None,
-                                  config=None)
-
-        return S3Transfer(connection, my_config)
+    # def get_s3_transfer(self):
+    #     # multipart_threshold=9999999999999999,  # workaround for 'disable' auto multipart upload
+    #     my_config = TransferConfig(
+    #         multipart_threshold=8000000,
+    #         max_concurrency=10,
+    #         num_download_attempts=10,
+    #     )
+    #
+    #     connection = boto3.client(service_name='s3',
+    #                               region_name='us-east-1',
+    #                               api_version=None,
+    #                               use_ssl=True,
+    #                               verify=True,
+    #                               aws_access_key_id=self.__access_key,
+    #                               aws_secret_access_key=self.__secret_key,
+    #                               aws_session_token=None,
+    #                               config=None)
+    #
+    #     return S3Transfer(connection, my_config)
 
     def key_exists(self, bucket_name, key_name):
-        s3 = self.get_s3_connection()
+        s3 = self.get_s3_resource()
         bucket = s3.Bucket(bucket_name)
         objs = list(bucket.objects.filter(Prefix=key_name))
         return len(objs) > 0 and objs[0].key == key_name
 
     def delete_a_key(self, bucket_name, key_name):
-        s3 = self.get_s3_connection()
+        s3 = self.get_s3_resource()
         bucket = s3.Bucket(bucket_name)
         objs = list(bucket.objects.filter(Prefix=key_name))
         for obj in objs:
             s3.Object(bucket.name, obj.key).delete()
 
     def get_all_keys(self, bucket_name):
-        s3 = self.get_s3_connection()
+        s3 = self.get_s3_resource()
         bucket = s3.Bucket(bucket_name)
         objs = list(bucket.objects.all())
         keys = []
@@ -106,7 +107,7 @@ class S3Accessor(object):
         return keys
 
     # def get_all_keys_by_msn(self, bucket_name, alia, msn):
-    #     s3 = self.get_s3_connection()
+    #     s3 = self.get_s3_resource()
     #     bucket = s3.Bucket(bucket_name)
     #     objs = list(bucket.objects.filter(Prefix=alia+'/'+msn))
     #     keys = []
@@ -116,7 +117,7 @@ class S3Accessor(object):
     #     return keys
     #
     # def get_all_keys_by_analysis_id(self, bucket_name, alia, analysis_id):
-    #     s3 = self.get_s3_connection()
+    #     s3 = self.get_s3_resource()
     #     bucket = s3.Bucket(bucket_name)
     #     objs = list(bucket.objects.filter(Prefix=alia))
     #     keys = []
@@ -126,7 +127,7 @@ class S3Accessor(object):
     #     return keys
 
     def bucket_exists(self, bucket_name):
-        s3 = self.get_s3_connection()
+        s3 = self.get_s3_resource()
         return s3.Bucket(bucket_name) in s3.buckets.all()
 
 
