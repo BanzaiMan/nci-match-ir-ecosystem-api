182551ee9c6acc795b298fe1917913cb41469d85
bcoffe <brent.coffey@nih.gov>
2016-08-29 07:47:13 -0400

    Added TODO's so everyone should know what needs to be done
    Wiki has highlights in orange of work to be done.
    https://wiki.nci.nih.gov/display/UM/Ecosystems+Services+and+Tables

--- resources/ion_reporter_record.py
+++ resources/ion_reporter_record.py
@@ -9,6 +9,8 @@ class IonReporterRecord(Resource):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
+    # TODO: Besides the querying of the ir table, add ability to get patients and/or sample controls sequenced
+    # for more information see https://wiki.nci.nih.gov/display/UM/Ecosystems+Services+and+Tables
     def get(self):
         abort(500, message="Not yet implemented")
 
