e9378d08e3f0f7b95b0bea456dce665e7dd424d1
wosman <waleed.a.osman@gmail.com>
2016-08-29 11:51:05 -0400

    Created GET for scanning ion reporters and return data

---
+++ accessors/ir_accessor.py
@@ -0,0 +1,57 @@
+import logging
+import __builtin__
+from dynamodb_accessor import DynamoDBAccessor
+
+KEY = 'ir_id'
+
+
+class IonReporterAccessor(DynamoDBAccessor):
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.debug("IonReporterAccessor instantiated")
+        self.logger.debug("__builtin__.environment: " + str(__builtin__.environment))
+        self.logger.debug("__builtin__.environment_config: " + str(__builtin__.environment_config))
+        DynamoDBAccessor.__init__(self, 'ion_reporters',
+                                  __builtin__.environment_config[__builtin__.environment]['dynamo_endpoint'],
+                                  __builtin__.environment_config[__builtin__.environment]['region'])
+
+    def create_table(self, table_name):
+        self.logger.info("Table " + table_name + " not found on system. Create_table called to create it")
+        try:
+            table = self.dynamodb.create_table(
+                TableName=table_name,
+                KeySchema=[{'AttributeName': 'ir_id', 'KeyType': 'HASH'}],
+                AttributeDefinitions=[{'AttributeName': 'ir_id', 'AttributeType': 'S'}],
+                ProvisionedThroughput={'ReadCapacityUnits': 5, 'WriteCapacityUnits': 5})
+            self.logger.info("Table " + table_name + " created!")
+        except Exception, e:
+            self.logger.error("create table exception in dynamodb: " + e.message)
+            raise
+
+        # wait for confirmation that the table exists
+        table.meta.client.get_waiter('table_exists').wait(TableName=table_name)
+        self.handle_table_creation(table_name)
+
+    def update(self, item_dictionary):
+        ir_table_key = str(item_dictionary.pop(KEY))
+        self.logger.debug(ir_table_key)
+        try:
+            return self.update_item(item_dictionary, dict(ir_id=ir_table_key))
+        except Exception, e:
+            self.logger.error("update_item exception in dynamodb: " + e.message)
+            raise
+
+    # In order to delete in Dynamodb, first call scan with the query parameters then loop over the returned values
+    # and call delete_item on each of them. You can't do deletes directly based on queries like you can in a RDBMS.
+    # So this is how you would do it here. The reason this is in the sample_control_accessor instead of in the
+    # dynamodb is because this function requires knowledge of the tables KEY so a generic means of doing this isn't
+    # as straight forward and is probably overly complicated for our purposes.
+    def batch_delete(self, query_parameters):
+        items_to_delete = self.scan(query_parameters)
+        for item in items_to_delete:
+            try:
+                self.delete_item({'ir_id': item[KEY]})
+            except Exception, e:
+                self.logger.error("Batch delete item exception in dynamodb: " + e.message)
+                raise
--- resources/ion_reporter_table.py
+++ resources/ion_reporter_table.py
@@ -1,5 +1,7 @@
 import logging
 from flask_restful import abort, request, reqparse, Resource
+from accessors.ir_accessor import IonReporterAccessor
+from common.dictionary_helper import DictionaryHelper
 
 parser = reqparse.RequestParser()
 # TODO: Create a means to store, retrieve, delete, and update information about the ion reporters themselves
@@ -7,11 +9,11 @@ parser = reqparse.RequestParser()
 parser.add_argument('ir_id', type=str, required=False)
 parser.add_argument('ip_address', type=str, required=False)
 parser.add_argument('internal_ip_address',         type=str, required=False)
-parser.add_argument('host_name',         type=str, required=False)
-parser.add_argument('site',         type=str, required=False)
-parser.add_argument('status',         type=str, required=False)
-parser.add_argument('last_contact',         type=str, required=False)
-parser.add_argument('data_files',         type=str, required=False)
+parser.add_argument('host_name', type=str, required=False)
+parser.add_argument('site', type=str, required=False)
+parser.add_argument('status', type=str, required=False)
+parser.add_argument('last_contact', type=str, required=False)
+parser.add_argument('data_files', type=str, required=False)
 
 class IonReporterTable(Resource):
     def __init__(self):
@@ -20,7 +22,17 @@ class IonReporterTable(Resource):
 # TODO begin here WAiO
 
     def get(self):
-
+        self.logger.info("Ion reporter GET called")
+        args = request.args
+        self.logger.debug(str(args))
+        try:
+            ion_reporter = IonReporterAccessor().scan(args) if DictionaryHelper.has_values(args) \
+                else IonReporterAccessor().scan(None)
+            self.logger.debug(str(ion_reporter))
+            return ion_reporter if ion_reporter is not None else \
+                abort(404, message="No ion reporters meet the query parameters")
+        except Exception, e:
+            self.logger.error("Get failed because: " + e.message)
             abort(500, message="Get failed because: " + e.message)
 
 
