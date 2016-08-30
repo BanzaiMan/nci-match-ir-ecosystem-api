1c3ef5ca530c15552d598c12f14260c472e7ebd0
wosman <waleed.a.osman@gmail.com>
2016-08-30 16:11:38 -0400

    Removed unused variable, and added testcase for generate Ion reporter id

---
+++ tests/test_ionReporterRecord.py
@@ -0,0 +1,23 @@
+from unittest import TestCase
+
+from ddt import ddt, data, unpack
+from mock import patch
+from accessors.ion_reporter_accessor import IonReporterAccessor
+
+import sys
+sys.path.append('..')
+
+@ddt
+class TestIonReporterRecord(TestCase):
+
+    @patch()
+    def test_get(self):
+
+
+        self.fail()
+
+    def test_put(self):
+        self.fail()
+
+    def test_delete(self):
+        self.fail()
