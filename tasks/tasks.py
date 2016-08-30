20ddb080e643d16af2f8ff5ef8c72ae5dc6481d0
yuq4 <qing.yu2@nih.gov>
2016-08-30 00:27:45 -0400

    add processing vcf to tsv, and updating sample_controls item in process_ir_file task

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -6,6 +6,9 @@ import yaml
 from accessors.ion_reporter_accessor import IonReporterAccessor
 from accessors.sample_control_accessor import SampleControlAccessor
 from celery import Celery
+from common.vcf_processor import VcfFileProcessor
+from accessors.s3_accessor import S3Accessor
+from common.s3_uploader import S3Uploader
 
 # Logging functionality
 logger = logging.getLogger(__name__)
@@ -50,8 +53,31 @@ def update_ir(update_message):
 @app.task
 def process_ir_file(file_process_message):
     logger.info("Processing file: " + str(file_process_message))
-    # TODO: process vcf and create tsv
+    # TODO: find out if bucket should come from update request message?
+    bucket = 'ped-match'
+
     # TODO: add tsv and bai paths, as appropriate, to update dictionary
+    if 'vcf_name' in file_process_message:
+        # upload vcf to S3
+        vcf_s3_path = S3Uploader(bucket, file_process_message).upload_file()
+        # convert vcf to tsv, then upload to s3
+        tsv_name = VcfFileProcessor().vcf_to_tsv(file_process_message['vcf_name'])
+        # replace 'vcf_name' item in file_process_message to 'tsv_name'
+        del file_process_message['vcf_name']
+        tsv_process_message = file_process_message.copy()
+        tsv_process_message.update({'tsv_name': tsv_name})
+        tsv_s3_path = S3Uploader(bucket, file_process_message).upload_file()
+
+        # prepare sample_controls table item update message
+        file_process_message.update({'vcf_name': vcf_s3_path})
+        file_process_message.update({'tsv_name': tsv_s3_path})
+
+    # elif 'dna_bam_name' in file_process_message:
+    #     # converting dna_bam to dna_bai
+    #     file_process_message.update({'dna_bai_name': dna_bai_name})
+    # elif 'cdna_bam_name' in file_process_message:
+    #     # converting cdna_bam to cdna_bai
+    #     file_process_message.update({'cdna_bai_name': cdna_bai_name})
 
     SampleControlAccessor().update(file_process_message)
 
