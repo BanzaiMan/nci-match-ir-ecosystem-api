8a531087d58ca0f1768099f90ac02eb3710c174b
wosman <waleed.a.osman@gmail.com>
2016-09-02 12:02:00 -0400

    Corrected version resource to return only version number

--- resources/version.py
+++ resources/version.py
@@ -1,12 +1,9 @@
 import logging
 from flask_restful import abort, request, Resource
-from flask import Response
 
 class Version(Resource):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
 
     def get(self):
-        resp = Response(status=200)
-        resp.headers['Version'] = '1.0'
-        return resp
+        return {'version':'1.0'}
