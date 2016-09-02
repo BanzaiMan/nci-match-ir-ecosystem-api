8816d1c86a93ea901eb969704a588fab0fcd184a
yuq4 <qing.yu2@nih.gov>
2016-09-01 23:56:56 -0400

    add download s3 file in get()

--- resources/sample_control_record.py
+++ resources/sample_control_record.py
@@ -1,9 +1,18 @@
 import logging
 from accessors.celery_task_accessor import CeleryTaskAccessor
 from accessors.sample_control_accessor import SampleControlAccessor
-from flask_restful import abort, Resource
+from flask_restful import abort, Resource, reqparse
 from flask import request
 from common.dictionary_helper import DictionaryHelper
+from accessors.s3_accessor import S3Accessor
+
+parser = reqparse.RequestParser()
+# Essential for POST, all other parameters are ignored on post except molecular_id, which, if passed in will cause a
+# failure. The proper order is to first POST to get a molecular_id then to PUT the files using the molecular_id in the
+# URI. From there other fields can be updated if needed.
+parser.add_argument('format',       type=str, required=False)  # format=zip
+parser.add_argument('file',         type=str, required=False)  # file=bam|bai|vcf|tsv|all
+parser.add_argument('type',         type=str, required=False)  # type=cdna|dna
 
 
 class SampleControlRecord(Resource):
@@ -47,17 +56,31 @@ class SampleControlRecord(Resource):
             self.logger.debug("delete_item failed because" + e.message)
             abort(500, message="delete_item item failed, because " + e.message)
 
-    # TODO: Expand GET functionality to include the retrieval of files from S3.
-    # see https://wiki.nci.nih.gov/display/UM/Ecosystems+Services+and+Tables for more information on the functionality
-    # that is needed to be added
+    # TODO: Expand GET functionality to zip downloaded s3 files and test.
+    # TODO: Confirm if new return dictionary is ok.
     def get(self, molecular_id):
         self.logger.info("Getting sample control with id: " + str(molecular_id))
+        args = parser.parse_args()
+        self.logger.debug("URL passed requested arguments: " + str(args))
+
         try:
             results = SampleControlAccessor().get_item({'molecular_id': molecular_id})
 
             if 'Item' in results:
                 self.logger.debug("Found: " + str(results['Item']))
-                return results['Item']
+
+                # download files from S3, if requested
+                request_download_filelist = self.__get_download_file_list(args)
+                self.logger.debug("Download s3 file list=" + str(request_download_filelist))
+                if len(request_download_filelist) > 0:
+                    downloaded_filelist = []
+                    for file_type in request_download_filelist:
+                        file_s3_path = results['Item'][file_type]
+                        downloaded_file_path = S3Accessor().download(file_s3_path)
+                        downloaded_filelist.append(downloaded_file_path)
+                    return {'item': results['Item'], 'downloaded_files': downloaded_filelist}
+                else:
+                    return results['Item']
 
         except Exception, e:
             self.logger.debug("get_item failed because" + e.message)
@@ -65,3 +88,37 @@ class SampleControlRecord(Resource):
 
         self.logger.info(molecular_id + " was not found")
         abort(404, message=str(molecular_id + " was not found"))
+
+    def __get_download_file_list(self, args):
+        download_file_list = []
+
+        if DictionaryHelper.has_values(args):
+            if args['file'] == 'bam' and args['type'] == 'dna':
+                download_file_list.append('dna_bam_name')
+            elif args['file'] == 'bam' and args['type'] == 'cdna':
+                download_file_list.append('cdna_bam_name')
+            elif args['file'] == 'bai' and args['type'] == 'dna':
+                download_file_list.append('dna_bai_name')
+            elif args['file'] == 'bai' and args['type'] == 'cdna':
+                download_file_list.append('cdna_bai_name')
+            elif args['file'] == 'bam':
+                download_file_list.append('dna_bam_name')
+                download_file_list.append('cdna_bam_name')
+            elif args['file'] == 'bai':
+                download_file_list.append('dna_bai_name')
+                download_file_list.append('cdna_bai_name')
+            elif args['file'] == 'vcf':
+                download_file_list.append('vcf_name')
+            elif args['file'] == 'tsv':
+                download_file_list.append('tsv_name')
+            elif args['file'] == 'all':
+                download_file_list.append('dna_bam_name')
+                download_file_list.append('dna_bai_name')
+                download_file_list.append('cdna_bam_name')
+                download_file_list.append('cdna_bai_name')
+                download_file_list.append('vcf_name')
+                download_file_list.append('tsv_name')
+            else:
+                self.logger.debug("No file requested to be downloaded from S3.")
+
+        return download_file_list
