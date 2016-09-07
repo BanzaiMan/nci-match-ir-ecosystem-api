8ee8e86296b4660fb33e478baa7006a9c0cdf596
bcoffe <brent.coffey@nih.gov>
2016-09-07 13:01:17 -0400

    changed syntax to comply with code climate.

--- resource_helpers/abort_logger.py
+++ resource_helpers/abort_logger.py
@@ -8,7 +8,7 @@ class AbortLogger(object):
     def log_and_abort(error_code, logger_level_function, message):
         try:
             calling_function = inspect.stack()[1][3]
-        except Exception, e:
+        except Exception as e:
             logger_level_function("Calling method not found in stack :: Log message: " + message + " :: " + e.message)
         else:
             logger_level_function("Calling Method: " + calling_function + " :: Log message: " + message)
