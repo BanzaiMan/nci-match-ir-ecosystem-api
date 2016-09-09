c312cc4781041b1660d1a7a1a9ff33660da4a306
wosman <waleed.a.osman@gmail.com>
2016-09-09 12:36:36 -0400

    Modified test_post function.

--- tests/test_sample_control_table.py
+++ tests/test_sample_control_table.py
@@ -50,17 +50,30 @@ class TestSampleControlTable(unittest.TestCase):
         assert "testing throwing exception" in return_value.data
         assert return_value.status_code == 500
 
-    @data(([{"molecular_id": "SC_SA1CB"}],'?site=mocha&control_type=no_template',
+    @data(('?site=mocha&control_type=no_template',
            '{"result": "New sample control created", "molecular_id":'),
-          (None, '', '{"message": "Sample Control creation failed, because both '
-                     'site and control_type were not passed in"}'))
+          ('?site=mocha', '{"message": "Sample Control creation failed, because both '
+                     'site and control_type were not passed in"}'),
+          ('?control_type=no_template', '{"message": "Sample Control creation failed, because both '
+                     'site and control_type were not passed in"}'),
+          ('', '{"message": "Sample Control creation failed, because both '
+                     'site and control_type were not passed in"}'),
+          ('?molecular_id=SC_WAO85', 'failed, because molecular_id'))
     @unpack
+    @patch('resources.sample_control_table.SampleControlTable.get_unique_key')
     @patch('accessors.sample_control_accessor.SampleControlAccessor.put_item')
-    def test_post(self, database_data, parameters, expected_results, mock_put_item_method):
-        mock_put_item_method.return_value = database_data
-        return_value = self.app.post('/api/v1/sample_controls' + parameters, content_type='application/json')
-        assert return_value.data.startswith(expected_results)
+    def test_post(self, parameters, expected_results, mock_put_item_method, mock_get_unique_key):
+        mock_put_item_method.return_value = True
+        mock_get_unique_key.return_value = 'SC_WAO85'
+        return_value = self.app.post('/api/v1/sample_controls' + parameters)
+        assert expected_results in return_value.data
 
+    @patch('accessors.sample_control_accessor.SampleControlAccessor.put_item')
+    def test_post_exception(self, mock_put_item_method):
+        mock_put_item_method.side_effect = Exception('Sample Control creation failed')
+        return_value = self.app.post('/api/v1/sample_controls?site=mocha&control_type=no_template')
+        assert return_value.status_code == 500
+        assert "Sample Control creation failed" in return_value.data
 
 if __name__ == '__main__':
     unittest.main()
