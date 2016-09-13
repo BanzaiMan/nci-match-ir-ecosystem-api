05e9a5f5036ba4d804e710f6eac0613141e28c3c
bcoffe <brent.coffey@nih.gov>
2016-09-13 10:10:31 -0400

    covering all lines of code in dictionary helper

--- tests/test_dictionaryHelper.py
+++ tests/test_dictionaryHelper.py
@@ -5,14 +5,21 @@ from common.dictionary_helper import DictionaryHelper
 
 @ddt
 class TestDictionaryHelper(unittest.TestCase):
-    @data(({'dic1': 'a'}, True), ('', False), ({}, False), (None, False))
+    @data(
+        ({'dic1': 'a'}, True),
+        ({'dic1': None}, False),
+        ('', False),
+        ({}, False),
+        (None, False))
     @unpack
     def test_has_values(self, dic1, expected_results):
         assert DictionaryHelper.has_values(dic1) == expected_results
 
     @data(
         ({'a': None, 'b': 1, 'c': 2}, {'a': 'b'}, False),
-        ({'a': None, 'b': 1, 'c': 2}, {'b': 'd'}, True),
+        ('', {'b': 'd'}, False),
+        ({'a': None, 'b': 1, 'c': 2}, None, False),
+        ({'a': 'x', 'b': 'y', 'c': 'z'}, {'q': 'c'}, False),
         ({'a': 'x', 'b': 'y', 'c': 'z'}, {'b': 'c'}, True)
     )
     @unpack
