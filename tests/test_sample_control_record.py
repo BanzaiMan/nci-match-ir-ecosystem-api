3f37c17d8b1c2e8440fff397b83acdc3d3b76fdd
yuq4 <qing.yu2@nih.gov>
2016-09-12 23:54:49 -0400

    update test case for test_get()

--- tests/test_sample_control_record.py
+++ tests/test_sample_control_record.py
@@ -12,16 +12,14 @@ class TestSampleControlRecord(unittest.TestCase):
         self.app = app.app.test_client()
 
     @data(
-        ({"site_ip_address": "129.43.127.133", "control_type": "no_template",
-          "dna_bam_name": "mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_DNA_v2.bam",
-          "site": "mocha", "qc_name": "None",
-          "dna_bai_name": "mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_DNA_v2.bai",
-          "tsv_name": "mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_v2.tsv",
-          "cdna_bam_name": "mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bam",
-          "vcf_name": "mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_v2.vcf",
-          "cdna_bai_name": "mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bai",
-          "date_molecular_id_created": "2016-08-28 16:56:29.333", "molecular_id": "SC_YQ111",
-          "analysis_id": "SC_YQ111_SC_YQ111_k123_v1"}, 'SC_YQ111', 200),
+        ({
+            "control_type": {"S": "no_template"},
+            "date_molecular_id_created": {"S": "2016-08-22 19:56:19.766"},
+            "site_ip_address": {"S": "129.43.127.133"},
+            "molecular_id": {"S": "SC_5AMCC"},
+            "site": {"S": "mocha"}
+         }, 'SC_5AMCC', 200
+        ),
         ({}, 'SC_YQ999', 404)
     )
     @unpack
@@ -30,6 +28,7 @@ class TestSampleControlRecord(unittest.TestCase):
         instance = mock_scAccessor_class.return_value
         instance.get_item.return_value = item
         return_value = self.app.get('/api/v1/sample_controls/' + molecular_id)
+        print "=============="+ str(return_value.status_code)
         assert return_value.status_code == expected_results
         if expected_results == 200:
             assert len(return_value.data) > 0
