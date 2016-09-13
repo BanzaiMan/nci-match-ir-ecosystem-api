cab016d35028d7809528421e7e7fe25a8bf050f2
yuq4 <qing.yu2@nih.gov>
2016-09-13 11:40:00 -0400

    patch source class

--- tests/test_sample_control_record.py
+++ tests/test_sample_control_record.py
@@ -13,11 +13,11 @@ class TestSampleControlRecord(unittest.TestCase):
 
     @data(
         ({
-            "control_type": {"S": "no_template"},
-            "date_molecular_id_created": {"S": "2016-08-28 16:56:29.333"},
-            "site_ip_address": {"S": "129.43.127.133"},
-            "molecular_id": {"S": "SC_YQ111"},
-            "site": {"S": "mocha"}
+             "control_type": {"S": "no_template"},
+             "date_molecular_id_created": {"S": "2016-08-28 16:56:29.333"},
+             "site_ip_address": {"S": "129.43.127.133"},
+             "molecular_id": {"S": "SC_YQ111"},
+             "site": {"S": "mocha"}
          }, 'SC_YQ111', 200
         ),
         ({
@@ -31,13 +31,12 @@ class TestSampleControlRecord(unittest.TestCase):
         ({}, 'SC_YQ999', 404)
     )
     @unpack
-    # TODO: I don't think this works. you have to mock the class where it is looked up not where it is defined.
-    @patch('accessors.sample_control_accessor.SampleControlAccessor')
-    def test_get(self, item, molecular_id, expected_results, mock_scAccessor_class):
-        instance = mock_scAccessor_class.return_value
-        instance.get_item.return_value = item
+    @patch('resources.sample_control_record.SampleControlRecord')
+    def test_get(self, item, molecular_id, expected_results, mock_scRecord_class):
+        instance = mock_scRecord_class.return_value
+        instance.SampleControlAccessor.get_item.return_value = item
         return_value = self.app.get('/api/v1/sample_controls/' + molecular_id)
-        print "=============="+ str(return_value.status_code)
+        print "==============" + str(return_value.status_code)
         assert return_value.status_code == expected_results
         if expected_results == 200:
             assert len(return_value.data) > 0
@@ -50,11 +49,10 @@ class TestSampleControlRecord(unittest.TestCase):
         ('SC_5AMCC', '"message": "Item deleted"')
     )
     @unpack
-    # TODO: I don't think this works. you have to mock the class where it is looked up not where it is defined.
-    @patch('accessors.celery_task_accessor.CeleryTaskAccessor')
-    def test_delete(self, molecular_id, expected_results, mock_celeryTaskAccessor_class):
-        instance = mock_celeryTaskAccessor_class.return_value
-        instance.update_item.return_value = True
+    @patch('resources.sample_control_record.SampleControlRecord')
+    def test_delete(self, molecular_id, expected_results, mock_scRecord_class):
+        instance = mock_scRecord_class.return_value
+        instance.CeleryTaskAccessor.delete_item.return_value = True
         return_value = self.app.delete('/api/v1/sample_controls/' + molecular_id)
         print "return data=" + str(return_value.data)
         assert return_value.data.find(expected_results)
@@ -62,18 +60,19 @@ class TestSampleControlRecord(unittest.TestCase):
     @data(
         ('SC_5AMCC',
          {'vcf_name': 'mocha/SC_5AMCC/SC_5AMCC_SC_5AMCC_k123_v1/SC_5AMCC_SC_5AMCC_analysis667_v1.vcf', 'site': 'mocha',
-          'molecular_id': 'SC_5AMCC', 'analysis_id': 'SC_5AMCC_SC_5AMCC_k123_v1'}, '"message": "Item updated"')
+          'molecular_id': 'SC_5AMCC', 'analysis_id': 'SC_5AMCC_SC_5AMCC_k123_v1'}, 'updated')
     )
     @unpack
-    # TODO: I don't think this works. you have to mock the class where it is looked up not where it is defined.
-    @patch('accessors.celery_task_accessor.CeleryTaskAccessor')
-    def test_put(self, molecular_id, update_dictionary, expected_results, mock_celeryTaskAccessor_class):
-        instance = mock_celeryTaskAccessor_class.return_value
-        instance.update_item.return_value = True
-        return_value = self.app.put('/api/v1/aliquot/' + molecular_id,
+    @patch('resources.sample_control_record.SampleControlRecord')
+    def test_put(self, molecular_id, update_dictionary, expected_results, mock_scRecord_class):
+        instance = mock_scRecord_class.return_value
+        instance.CeleryTaskAccessor.update_item(update_dictionary).return_value = True
+        return_value = self.app.put('/api/v1/sample_controls/' + molecular_id,
                                     data=json.dumps(update_dictionary),
                                     content_type='application/json')
-        assert return_value.data.find(expected_results)
+        print return_value.data
+        assert return_value.data.find(molecular_id + " " + expected_results)
+
 
 if __name__ == '__main__':
     unittest.main()
