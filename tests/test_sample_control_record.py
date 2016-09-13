c3aaaebd223546a465f052c1152183f7cc4bcef9
yuq4 <qing.yu2@nih.gov>
2016-09-13 13:42:45 -0400

    add test_get_exception

--- tests/test_sample_control_record.py
+++ tests/test_sample_control_record.py
@@ -13,31 +13,25 @@ class TestSampleControlRecord(unittest.TestCase):
 
     @data(
         ({
-             "control_type": {"S": "no_template"},
-             "date_molecular_id_created": {"S": "2016-08-28 16:56:29.333"},
-             "site_ip_address": {"S": "129.43.127.133"},
-             "molecular_id": {"S": "SC_YQ111"},
-             "site": {"S": "mocha"}
+             "control_type": "no_template",
+             "date_molecular_id_created": "2016-08-28 16:56:29.333",
+             "site_ip_address": "129.43.127.133",
+             "molecular_id": "SC_YQ111",
+             "site": "mocha"
          }, 'SC_YQ111', 200
-        ),
-        ({
-             "control_type": {"S": "no_template"},
-             "date_molecular_id_created": {"S": "2016-08-22 19:56:19.766"},
-             "site_ip_address": {"S": "129.43.127.133"},
-             "molecular_id": {"S": "SC_5AMCC"},
-             "site": {"S": "mocha"}
-         }, 'SC_5AMCC', 200
         ),
         ({}, 'SC_YQ999', 404)
     )
     @unpack
-    @patch('resources.sample_control_record.SampleControlRecord')
+    @patch('resources.sample_control_record.SampleControlAccessor')
     def test_get(self, item, molecular_id, expected_results, mock_scRecord_class):
         instance = mock_scRecord_class.return_value
-        instance.SampleControlAccessor.get_item.return_value = item
+        # instance.SampleControlAccessor.get_item.return_value = item
+        instance.get_item.return_value = item
         return_value = self.app.get('/api/v1/sample_controls/' + molecular_id)
         print "==============" + str(return_value.status_code)
         assert return_value.status_code == expected_results
+        print return_value.data
         if expected_results == 200:
             assert len(return_value.data) > 0
             assert (json.loads(return_value.data))['site'] == "mocha"
@@ -45,14 +39,23 @@ class TestSampleControlRecord(unittest.TestCase):
             print return_value.data
             assert return_value.data.find("message")
 
+    @patch('resources.sample_control_record.SampleControlAccessor')
+    def test_get_exception(self, mock_scRecord_class):
+        instance = mock_scRecord_class.return_value
+        instance.get_item.side_effect = Exception('Testing get_item exception')
+        return_value = self.app.get('/api/v1/sample_controls/SC_5AMCC')
+        print return_value.status_code
+        assert return_value.status_code == 500
+        assert "Testing get_item exception" in return_value.data
+
     @data(
         ('SC_5AMCC', '"message": "Item deleted"')
     )
     @unpack
-    @patch('resources.sample_control_record.SampleControlRecord')
+    @patch('resources.sample_control_record.CeleryTaskAccessor')
     def test_delete(self, molecular_id, expected_results, mock_scRecord_class):
         instance = mock_scRecord_class.return_value
-        instance.CeleryTaskAccessor.delete_item.return_value = True
+        instance.delete_item.return_value = True
         return_value = self.app.delete('/api/v1/sample_controls/' + molecular_id)
         print "return data=" + str(return_value.data)
         assert return_value.data.find(expected_results)
@@ -63,10 +66,10 @@ class TestSampleControlRecord(unittest.TestCase):
           'molecular_id': 'SC_5AMCC', 'analysis_id': 'SC_5AMCC_SC_5AMCC_k123_v1'}, 'updated')
     )
     @unpack
-    @patch('resources.sample_control_record.SampleControlRecord')
+    @patch('resources.sample_control_record.CeleryTaskAccessor')
     def test_put(self, molecular_id, update_dictionary, expected_results, mock_scRecord_class):
         instance = mock_scRecord_class.return_value
-        instance.CeleryTaskAccessor.update_item(update_dictionary).return_value = True
+        instance.update_item(update_dictionary).return_value = True
         return_value = self.app.put('/api/v1/sample_controls/' + molecular_id,
                                     data=json.dumps(update_dictionary),
                                     content_type='application/json')
