ef760b1e0a311ee4cd2f35cb8e2edff0a0bb27b8
yuq4 <qing.yu2@nih.gov>
2016-09-14 16:01:49 -0400

    add test_upload and test_upload_exception

--- tests/test_s3_accessor.py
+++ tests/test_s3_accessor.py
@@ -6,8 +6,46 @@ from accessors.s3_accessor import S3Accessor
 
 @ddt
 class TestS3Accessor(unittest.TestCase):
+
     def setUp(self):
         self.app = app.app.test_client()
+
+
+    @data(
+        ('/tmp/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bam',
+         'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bam')
+    )
+    @unpack
+    @patch('accessors.s3_accessor.boto3')
+    def test_upload(self, local_file_path, file_s3_path, mock_boto3):
+        instance = mock_boto3.return_value
+        instance.client.return_value = True
+        instance.resource.return_value = True
+        s3_accessor = S3Accessor()
+        s3_accessor.resource.meta.client.upload_file.return_value = True
+        return_value = s3_accessor.upload(local_file_path, file_s3_path)
+        print "===============" + str(return_value)
+        assert (return_value == None)
+
+
+    @data(
+        ('/tmp/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bam',
+         'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bam',
+         'Testing s3 upload exception')
+    )
+    @unpack
+    @patch('accessors.s3_accessor.boto3')
+    def test_upload_exception(self, local_file_path, file_s3_path, exception_message, mock_boto3):
+        instance = mock_boto3.return_value
+        instance.client.return_value = True
+        instance.resource.return_value = True
+        s3_accessor = S3Accessor()
+        s3_accessor.resource.meta.client.upload_file.side_effect = Exception(exception_message)
+        try:
+            s3_accessor.upload(local_file_path, file_s3_path)
+        except Exception as e:
+            assert (e.message == exception_message)
+
 
     @data(
             ('mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bam',
