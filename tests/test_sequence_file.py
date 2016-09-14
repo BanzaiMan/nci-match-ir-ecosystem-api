8ca3bebb6b67d54d6d1c81b47d67282c1175cce8
yuq4 <qing.yu2@nih.gov>
2016-09-13 22:42:07 -0400

    add test exceptions

--- tests/test_sequence_file.py
+++ tests/test_sequence_file.py
@@ -67,8 +67,112 @@ class TestSequenceFile(unittest.TestCase):
             return_value = self.app.get(
                 '/api/v1/sequence_files/' + molecular_id + '/' + file_format + '/' + nucleic_acid_type)
 
-        print return_value.data
         assert (json.loads(return_value.data)["s3_download_file_url"] == expect_return)
+
+    @data(
+            ('SC_YQ111', 'vcf', None),
+            ('SC_YQ111', 'bam', 'cdna')
+        )
+    @unpack
+    @patch('resources.sequence_file.SampleControlAccessor')
+    def test_get_item_exception(self, molecular_id, file_format, nucleic_acid_type, mock_sc_accessor):
+        instance = mock_sc_accessor.return_value
+        instance.get_item.side_effect = Exception('Testing get_item exception')
+
+        if nucleic_acid_type is None:
+            return_value = self.app.get('/api/v1/sequence_files/' + molecular_id + '/' + file_format)
+        else:
+            return_value = self.app.get(
+                '/api/v1/sequence_files/' + molecular_id + '/' + file_format + '/' + nucleic_acid_type)
+
+        assert return_value.status_code == 500
+        assert "Testing get_item exception" in return_value.data
+
+
+    @data(
+            ('SC_YQ111', 'vcf', None,
+             {'site_ip_address': '129.43.127.133',
+               'control_type': 'no_template',
+               'site': 'mocha',
+               'vcf_name': 'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_v2.vcf',
+               'date_molecular_id_created': '2016-08-28 16:56:29.333',
+               'molecular_id': 'SC_YQ111',
+               'analysis_id': 'SC_YQ111_SC_YQ111_k123_v1'
+             }
+            ),
+            ('SC_YQ111', 'bam', 'cdna',
+             {'site_ip_address': '129.43.127.133',
+               'control_type': 'no_template',
+               'site': 'mocha',
+               'cdna_bam_name': 'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bam',
+               'date_molecular_id_created': '2016-08-28 16:56:29.333',
+               'molecular_id': 'SC_YQ111',
+               'analysis_id': 'SC_YQ111_SC_YQ111_k123_v1'
+             }
+            )
+         )
+    @unpack
+    @patch('resources.sequence_file.SampleControlAccessor')
+    @patch('resources.sequence_file.S3Accessor')
+    def test_get_file_url_exception(self, molecular_id, file_format, nucleic_acid_type, item,
+                                                 mock_s3_accessor, mock_sc_accessor):
+        s3_instance=mock_s3_accessor.return_value
+        s3_instance.get_download_url.side_effect = Exception('Testing get_download_url exception')
+        instance = mock_sc_accessor.return_value
+        instance.get_item.return_value = item
+
+
+        if nucleic_acid_type is None:
+            return_value = self.app.get('/api/v1/sequence_files/' + molecular_id + '/' + file_format)
+        else:
+            return_value = self.app.get(
+                '/api/v1/sequence_files/' + molecular_id + '/' + file_format + '/' + nucleic_acid_type)
+
+        assert return_value.status_code == 500
+        assert "Testing get_download_url exception" in return_value.data
+
+    @data(
+            ('SC_YQ111', 'tsv', None,
+             {'site_ip_address': '129.43.127.133',
+              'control_type': 'no_template',
+              'site': 'mocha',
+              'tsv_name': 'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_v2.tsv',
+              'cdna_bai_name': 'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bai',
+              'date_molecular_id_created': '2016-08-28 16:56:29.333',
+              'molecular_id': 'SC_YQ111',
+              'analysis_id': 'SC_YQ111_SC_YQ111_k123_v1'
+              }
+             ),
+            ('SC_YQ111', 'bai', 'cdna',
+             {'site_ip_address': '129.43.127.133',
+              'control_type': 'no_template',
+              'site': 'mocha',
+              'tsv_name': 'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_v2.tsv',
+              'cdna_bai_name': 'mocha/SC_YQ111/SC_YQ111_SC_YQ111_k123_v1/SC_YQ111_SC_YQ111_analysis666_RNA_v2.bai',
+              'date_molecular_id_created': '2016-08-28 16:56:29.333',
+              'molecular_id': 'SC_YQ111',
+              'analysis_id': 'SC_YQ111_SC_YQ111_k123_v1'
+              }
+             )
+        )
+    @unpack
+    @patch('resources.sequence_file.SampleControlAccessor')
+    @patch('resources.sequence_file.S3Accessor')
+    def test_get_file_url_key_error_exception(self, molecular_id, file_format, nucleic_acid_type, item,
+                                    mock_s3_accessor, mock_sc_accessor):
+        s3_instance = mock_s3_accessor.return_value
+        s3_instance.get_download_url.side_effect = KeyError('Testing get_download_url s3 key')
+        instance = mock_sc_accessor.return_value
+        instance.get_item.return_value = item
+
+        if nucleic_acid_type is None:
+            return_value = self.app.get('/api/v1/sequence_files/' + molecular_id + '/' + file_format)
+        else:
+            return_value = self.app.get(
+                '/api/v1/sequence_files/' + molecular_id + '/' + file_format + '/' + nucleic_acid_type)
+
+        assert return_value.status_code == 404
+        assert "Testing get_download_url s3 key" in return_value.data
 
 if __name__ == '__main__':
     unittest.main()
