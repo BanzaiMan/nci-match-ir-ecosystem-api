1e49d50af7ab5bc2baabc834ddca30c83cc5cf4e
bcoffe <brent.coffey@nih.gov>
2016-09-14 08:34:23 -0400

    refomormated file

--- tests/test_query_helper.py
+++ tests/test_query_helper.py
@@ -6,7 +6,6 @@ from common.query_helper import QueryHelper
 
 @ddt
 class TestQueryHelper(unittest.TestCase):
-
     @data(('site', 'MoCha', 'control_type', 'positive'))
     @unpack
     def test_create_key_dict(self, key1, value1, key2, value2):
@@ -15,9 +14,9 @@ class TestQueryHelper(unittest.TestCase):
         # key2 = 'control_type'
         # value2 = 'positive'
         key_dict = {key1: value1, key2: value2}
-        #print key_dict
+        # print key_dict
         filter_condition = QueryHelper.create_filter_expression(key_dict)
-        #expected = Attr(key1).eq(value1) & Attr(key2).eq(value2)
+        # expected = Attr(key1).eq(value1) & Attr(key2).eq(value2)
         # print filter_condition.get_expression() = expected.get_expression()
         # print filter_condition.expression_format()
         # print filter_condition.
@@ -26,9 +25,6 @@ class TestQueryHelper(unittest.TestCase):
         assert filter_condition.get_expression()['operator'] == 'AND'
         assert str(filter_condition.get_expression()['values']).count('boto3.dynamodb.conditions.Equals object at') == 2
         assert filter_condition.get_expression()['format'] == '({0} {operator} {1})'
-
-
-
 
 
 if __name__ == '__main__':
