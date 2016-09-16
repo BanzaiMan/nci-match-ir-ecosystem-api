d39ba7e13d50921ad2b56e0c1040b0c0e6e66f45
wosman <waleed.a.osman@gmail.com>
2016-09-16 14:44:21 -0400

    Testing sequence_data.py.

--- tests/test_sequence_data.py
+++ tests/test_sequence_data.py
@@ -14,20 +14,45 @@ class TestSequenceData(unittest.TestCase):
         pass
 
     @data(
+        ('patients',
+         'IR_WAO85', {u'message': u'Finding patients sequenced with IR: IR_WAO85 not yet implemented.'}, 501),
+        ('waleeds',
+         'IR_WAO85', {u'message': u'Can only request patients or sample_controls. You requested: waleeds. '
+                                  u'You have requested this URI [/api/v1/ion_reporters/IR_WAO85/waleeds] but did you '
+                                  u'mean /api/v1/ion_reporters/<string:ion_reporter_id> or '
+                                  u'/api/v1/ion_reporters/version or /api/v1/ion_reporters ?'}, 404),
         ('sample_controls',
-         'IR_WAO85', [{"ion_reporter_id": "IR_WAO85", "molecular_id": "SC_SA1CB", "site": "mocha", "control_type": "no_template", "date_molecular_id_created": "2016-08-18 19:56:19.766"}, {"ion_reporter_id": "IR_WAO85", "molecular_id": "SC_SAWCB", "site": "mocha", "control_type": "no_template", "date_molecular_id_created": "2016-08-18 19:56:19.766"}, {"ion_reporter_id": "IR_WAO85", "molecular_id": "SC_67VKV", "site": "mocha", "control_type": "positive", "date_molecular_id_created": "2016-08-18 20:09:45.667"}], 200),
+         'IR_WAO85', [
+             {"ion_reporter_id": "IR_WAO85", "molecular_id": "SC_SA1CB", "site": "mocha", "control_type": "no_template",
+              "date_molecular_id_created": "2016-08-18 19:56:19.766"},
+             {"ion_reporter_id": "IR_WAO85", "molecular_id": "SC_SAWCB", "site": "mocha", "control_type": "no_template",
+              "date_molecular_id_created": "2016-08-18 19:56:19.766"},
+             {"ion_reporter_id": "IR_WAO85", "molecular_id": "SC_67VKV", "site": "mocha", "control_type": "positive",
+              "date_molecular_id_created": "2016-08-18 20:09:45.667"}], 200),
 
     )
     @unpack
-    def test_get_file_url(self, sequence_data, ion_reporter_id, expected_return, returned_stat_code, mock_sc_accessor):
+    def test_get(self, sequence_data, ion_reporter_id, expected_return, returned_stat_code, mock_sc_accessor):
         sc_instance = mock_sc_accessor.return_value
         sc_instance.scan.return_value = expected_return
-
-
         return_value = self.app.get('/api/v1/ion_reporters/' + ion_reporter_id + '/' + sequence_data)
-        print "==============" + str(return_value.status_code)
+        print return_value.status_code
+        print json.loads(return_value.data)
         assert return_value.status_code == returned_stat_code
-        print "-----------------" +return_value.data
-        print str(expected_return)
-        print type(return_value.data)
-        #assert str(expected_return) == return_value.data
+        assert expected_return == json.loads(return_value.data)
+
+
+    @data(
+        ('sample_controls',
+         'IR_WAO85', 'Server Error contact help', 500),
+
+    )
+    @unpack
+    def test_get_exception(self, sequence_data, ion_reporter_id, expected_return, returned_stat_code, mock_sc_accessor):
+        sc_instance = mock_sc_accessor.return_value
+        sc_instance.scan.side_effect = Exception(expected_return)
+        return_value = self.app.get('/api/v1/ion_reporters/' + ion_reporter_id + '/' + sequence_data)
+        print return_value.status_code
+        print return_value.data
+        assert return_value.status_code == returned_stat_code
+        assert expected_return in return_value.data
