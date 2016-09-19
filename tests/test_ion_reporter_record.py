1088e0f8666dc7d9e83d1cfbd4cf50b1ebef0a5d
wosman <waleed.a.osman@gmail.com>
2016-09-19 16:40:44 -0400

    Updated dictionary helper test; 100% coverage.

--- tests/test_ion_reporter_record.py
+++ tests/test_ion_reporter_record.py
@@ -6,25 +6,30 @@ from mock import patch
 
 
 @ddt
-class TestIonReporterTable(unittest.TestCase):
+class TestIonReporterRecord(unittest.TestCase):
     def setUp(self):
         # Starting flask server
         self.app = app.app.test_client()
 
     @data(
-            ({"status": "Contacted 5 minutes ago", "last_contact": "August 29, 2016 2:00 PM GMT"}, 'IR_WO3IA', 200),
-            ({}, 'IR_WO4IA', 404)
+            ([{}, {}], 'IR_AIO78', {'projection': ['site', 'ir_status']}, 200),
+            ({}, 'IR_WO4IA', None, 404),
+            ({}, '', None, 404),
+            ([{}, {}], 'IR_AIO78', {'projection': None}, 200),
     )
     @unpack
     @patch('resources.ion_reporter_record.IonReporterAccessor')
-    def test_get(self, database_data, ion_reporter_id, expected_results, mock_class):
+    @patch('resources.sequence_data.reqparse.RequestParser.parse_args')
+    def test_get(self, database_data, ion_reporter_id, args, expected_results, mock_parse_args_function, mock_class):
         instance = mock_class.return_value
         instance.scan.return_value = database_data
+        mock_parse_args_function.return_value = args
         return_value = self.app.get('/api/v1/ion_reporters/' + ion_reporter_id)
         assert return_value.status_code == expected_results
         if expected_results == 200:
             assert len(return_value.data) > 0
-            assert (json.loads(return_value.data))['status'] == "Contacted 5 minutes ago"
+            print json.loads(return_value.data)
+            assert (json.loads(return_value.data)) == database_data
         else:
             assert return_value.data.find("message")
 
