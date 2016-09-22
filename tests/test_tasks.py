35e80bf0aaa20961e614da57a3ba32b1d1776d42
yuq4 <qing.yu2@nih.gov>
2016-09-22 17:00:17 -0400

    add test_process_file_message_no_process_exception

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -15,57 +15,76 @@ class TestTasks(unittest.TestCase):
         pass
 
     @data(
-            ({'site_ip_address': '129.43.127.133',
-              'control_type': 'no_template',
-              'site': 'mocha',
-              "ion_reporter_id": "IR_WAO85",
-              'vcf_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.vcf',
-              'molecular_id': 'SC_SA1CB',
-              'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
-              },
-             'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.tsv',
-             {'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1', 'control_type': 'no_template', 'site': 'mocha',
-              "ion_reporter_id": "IR_WAO85",
-              'tsv_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.tsv',
-              'vcf_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.vcf',
-              'molecular_id': 'SC_SA1CB', 'site_ip_address': '129.43.127.133'}
-            ),
-            ({'site_ip_address': '129.43.127.133',
-              'control_type': 'no_template',
-              'site': 'mocha',
-              "ion_reporter_id": "IR_WAO85",
-              'dna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
-              'molecular_id': 'SC_SA1CB',
-              'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
-              },
-             'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
-             {'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1', 'control_type': 'no_template',
-              'dna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
-              'site': 'mocha',
-              "ion_reporter_id": "IR_WAO85",
-              'dna_bai_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
-              'molecular_id': 'SC_SA1CB', 'site_ip_address': '129.43.127.133'}
-            ),
-            ({'site_ip_address': '129.43.127.133',
-              'control_type': 'no_template',
-              'site': 'mocha',
-              "ion_reporter_id": "IR_WAO85",
-              'cdna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
-              'molecular_id': 'SC_SA1CB',
-              'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
-              },
-             'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
-             {'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1', 'control_type': 'no_template', 'site': 'mocha',
-              "ion_reporter_id": "IR_WAO85",
-              'cdna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
-              'cdna_bai_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
-              'molecular_id': 'SC_SA1CB', 'site_ip_address': '129.43.127.133'}
-            )
+        ({'site_ip_address': '129.43.127.133',
+          'control_type': 'no_template',
+          'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'vcf_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.vcf',
+          'molecular_id': 'SC_SA1CB',
+          'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
+          },
+         'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.tsv',
+         {'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1', 'control_type': 'no_template', 'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'tsv_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.tsv',
+          'vcf_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.vcf',
+          'molecular_id': 'SC_SA1CB', 'site_ip_address': '129.43.127.133'}
+         ),
+        ({'site_ip_address': '129.43.127.133',
+          'control_type': 'no_template',
+          'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'dna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
+          'molecular_id': 'SC_SA1CB',
+          'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
+          },
+         'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
+         {'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1', 'control_type': 'no_template',
+          'dna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
+          'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'dna_bai_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
+          'molecular_id': 'SC_SA1CB', 'site_ip_address': '129.43.127.133'}
+         ),
+        ({'site_ip_address': '129.43.127.133',
+          'control_type': 'no_template',
+          'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'cdna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
+          'molecular_id': 'SC_SA1CB',
+          'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
+          },
+         'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
+         {'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1', 'control_type': 'no_template', 'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'cdna_bam_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bam',
+          'cdna_bai_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.bai',
+          'molecular_id': 'SC_SA1CB', 'site_ip_address': '129.43.127.133'}
+         ),
+        ({'site_ip_address': '129.43.127.133',
+          'control_type': 'no_template',
+          'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'qc_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.pdf',
+          'molecular_id': 'SC_SA1CB',
+          'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
+          },
+         '',
+         {'site_ip_address': '129.43.127.133',
+          'control_type': 'no_template',
+          'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'qc_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.pdf',
+          'molecular_id': 'SC_SA1CB',
+          'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
+          }
          )
+    )
     @unpack
     @patch('tasks.tasks.S3Accessor')
     @patch('tasks.tasks.SequenceFileProcessor')
-    def test_process_file_message(self, file_process_message, new_file_path, expected_return, mock_sf_accessor_class, mock_s3_accessor_class):
+    def test_process_file_message(self, file_process_message, new_file_path, expected_return, mock_sf_accessor_class,
+                                  mock_s3_accessor_class):
 
         s3_instance = mock_s3_accessor_class.return_value
         s3_instance.download.return_value = True
@@ -77,6 +96,38 @@ class TestTasks(unittest.TestCase):
         return_value = tasks.process_file_message(file_process_message)
         print "===================" + str(return_value)
         assert (return_value == expected_return)
+
+    @data(
+        ({'site_ip_address': '129.43.127.133',
+          'control_type': 'no_template',
+          'site': 'mocha',
+          "ion_reporter_id": "IR_WAO85",
+          'vcf_name': 'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.vcf',
+          'molecular_id': 'SC_SA1CB',
+          'analysis_id': 'SC_SA1CB_SC_SA1CB_a888_v1'
+          },
+         "Testing in process_file_message s3 download failed"
+         )
+    )
+    @unpack
+    @patch('tasks.tasks.S3Accessor')
+    @patch('tasks.tasks.SequenceFileProcessor')
+    def test_process_file_message_no_process_exception(self, file_process_message, exception_message,
+                                                       mock_sf_accessor_class,
+                                                       mock_s3_accessor_class):
+        s3_instance = mock_s3_accessor_class.return_value
+        s3_instance.download.side_effect = Exception(exception_message)
+        s3_instance.upload.return_value = True
+        sf_instance = mock_sf_accessor_class.return_value
+        sf_instance.vcf_to_tsv.return_value = True
+        sf_instance.bam_to_bai.return_value = True
+
+        try:
+            tasks.process_file_message(file_process_message)
+        except Exception as e:
+            print "==================" + e.message
+            assert exception_message in e.message
+
 
     #TODO add test exception, and test else ...
 
