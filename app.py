c3d1044ebc4fd7b43413211dfd88194d62705e7e
bcoffe <brent.coffey@nih.gov>
2016-09-30 08:16:23 -0400

    added comments to be more explicit

--- app.py
+++ app.py
@@ -43,33 +43,38 @@ EnvironmentHelper.set_environment(logger.info)
 # plural classes.
 
 # Path for Querying and Creating sample controls
+# Delete and Put in batch must do those using specific IDs, not allowed here
 api.add_resource(SampleControlTable, '/api/v1/sample_controls')
 
-# Path for Updating and deleting a specific sample control
+# Path for Updating, deleting, and retrieving a specific sample control
 api.add_resource(SampleControlRecord, '/api/v1/sample_controls/<string:identifier>')
 
 # Paths for downloading vcf or tsv file, format = vcf|tsv
 api.add_resource(VariantSequenceFile, '/api/v1/sequence_files/<string:molecular_id>/<string:file_format>')
-# TODO: still working on this
-api.add_resource(S3AuthenticationPolicy,
-                 '/api/v1/files/<string:molecular_id>/<string:analysis_id>/<string:file_name>')
-
 # Paths for downloading bam or bai file, format = bam|bai, type=cdna|dna
 api.add_resource(AlignmentSequenceFile,
                  '/api/v1/sequence_files/<string:molecular_id>/<string:file_format>/<string:nucleic_acid_type>')
 
-# Paths for updating information about ion reporters themselves
-api.add_resource(IonReporterTable, '/api/v1/ion_reporters')
-api.add_resource(IonReporterRecord, '/api/v1/ion_reporters/<string:identifier>')
-api.add_resource(SequenceData, '/api/v1/ion_reporters/<string:ion_reporter_id>/<string:sequence_data>')
+# TODO: still working on this but this will be used to authenticate a user to upload to S3
+api.add_resource(S3AuthenticationPolicy,
+                 '/api/v1/files/<string:molecular_id>/<string:analysis_id>/<string:file_name>')
 
-# Path for either sending in files from the ir (bam,vcf) to process them and store them based on their molecular id
-# in either the patient or sample_control table. Also a path to query with GET to see if molecular_id is valid and
-# what type of molecular id it is (i.e., sample_control or patient)
-api.add_resource(Aliquot, '/api/v1/aliquot/<string:molecular_id>')
+# Path for Querying and Creating ion reporters
+# Delete and Put in batch must do those using specific IDs, not allowed here
+api.add_resource(IonReporterTable, '/api/v1/ion_reporters')
+# Paths for updating, deleting, and retrieveing information about ion reporters themselves
+api.add_resource(IonReporterRecord, '/api/v1/ion_reporters/<string:identifier>')
+# Retrieves a list of all the patients or sample_controls sequenced on the given IR reporter
+api.add_resource(SequenceData, '/api/v1/ion_reporters/<string:ion_reporter_id>/<string:sequence_data>')
 
 # Path for version call, returning 200 as test response
 api.add_resource(Version, '/api/v1/ion_reporters/version')
+
+# Path for either sending in file paths from the ir (bam,vcf) to process them by pulling them down from s3
+# and store the new files to s3 and their paths, based on their molecular id, in either the patient or sample_control
+# table. Also a path to query with GET to see if molecular_id is valid and what type of molecular id it is
+# (i.e., sample_control or patient)
+api.add_resource(Aliquot, '/api/v1/aliquot/<string:molecular_id>')
 
 # For the most part, this is boilerplate code to start tornado server
 if __name__ == '__main__':
