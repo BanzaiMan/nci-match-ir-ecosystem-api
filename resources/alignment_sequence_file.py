4d23ca30d2b91079ed42e3e6abc62eb56672100b
bcoffe <brent.coffey@nih.gov>
2016-09-30 08:58:08 -0400

    making code more elegant and concise.

--- resources/alignment_sequence_file.py
+++ resources/alignment_sequence_file.py
@@ -1,12 +1,14 @@
 from resources.sequence_file import SequenceFile
 from resource_helpers.abort_logger import AbortLogger
 
+valid_file_formats = ['bam', 'bai']
+valid_nucleic_acid_types = ['cdna', 'dna']
+
 
 class AlignmentSequenceFile(SequenceFile):
 
     def get(self, molecular_id, file_format, nucleic_acid_type):
-        if (file_format.lower() == 'bam' or file_format.lower() == 'bai') and \
-                (nucleic_acid_type.lower() == 'cdna' or nucleic_acid_type.lower() == 'dna'):
+        if (file_format.lower() in valid_file_formats) and (nucleic_acid_type.lower() in valid_nucleic_acid_types):
             return self.get_file_url(molecular_id, str(nucleic_acid_type) + "_" + str(file_format) + "_name")
 
         AbortLogger.log_and_abort(400, self.logger.error, "Only supports bam|bai and cdna|dna")
--- resources/variant_sequence_file.py
+++ resources/variant_sequence_file.py
@@ -1,11 +1,13 @@
 from resources.sequence_file import SequenceFile
 from resource_helpers.abort_logger import AbortLogger
 
+valid_file_formats = ['vcf', 'tsv']
+
 
 class VariantSequenceFile(SequenceFile):
 
     def get(self, molecular_id, file_format):
-        if file_format.lower() == 'vcf' or file_format.lower() == 'tsv':
+        if file_format.lower() in valid_file_formats:
             return self.get_file_url(molecular_id, str(file_format) + "_name")
 
         AbortLogger.log_and_abort(400, self.logger.error, "Only supports vcf and tsv files")
