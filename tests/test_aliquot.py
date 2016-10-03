34abe4fd9599ca54237db4b5bbc1adc0ba88adb3
yuq4 <qing.yu2@nih.gov>
2016-10-03 11:46:30 -0400

    mock verify_molecular_id() for patient in test_get()

--- tests/test_aliquot.py
+++ tests/test_aliquot.py
@@ -10,32 +10,53 @@ class TestAliquot(unittest.TestCase):
     def setUp(self):
         self.app = app.app.test_client()
 
-    # @data(
-    #     ({
-    #          "control_type": "no_template",
-    #          "date_molecular_id_created": "2016-08-28 16:56:29.333",
-    #          "site_ip_address": "129.43.127.133",
-    #          "molecular_id": "SC_YQ111",
-    #          "ion_reporter_id": "IR_WAO85",
-    #          "site": "mocha"
-    #      },
-    #      'SC_YQ111', 200),
-    #     ({}, 'SC_YQ999', 404)
-    # )
-    # @unpack
-    # @patch('resources.aliquot.SampleControlAccessor')
-    # def test_get(self, item, molecular_id, expected_results, mock_aliquot_class):
-    #     instance = mock_aliquot_class.return_value
-    #     instance.get_item.return_value = item
-    #     return_value = self.app.get('/api/v1/aliquot/' + molecular_id)
-    #     print "==============" + str(return_value.status_code)
-    #     assert return_value.status_code == expected_results
-    #     print return_value.data
-    #     if expected_results == 200:
-    #         assert len(return_value.data) > 0
-    #         assert (json.loads(return_value.data))['site'] == "mocha"
-    #     else:
-    #         assert return_value.data.find(molecular_id + " was not found.")
+    @data(
+        ({
+             "control_type": "no_template",
+             "date_molecular_id_created": "2016-08-28 16:56:29.333",
+             "site_ip_address": "129.43.127.133",
+             "molecular_id": "SC_YQ111",
+             "ion_reporter_id": "IR_WAO85",
+             "site": "mocha"
+         },
+         'SC_YQ111', {}, 200),
+        ({}, 'SC_YQ999', {}, 404),
+        ({}, 'PT_SR10_BdVRRejected_BD_MOI1', [{
+            "uuid": "69cf6ae8-d95b-463a-be4e-1ba2b7ebdf04",
+            "shipped_date": "2016-05-01T19:42:13+00:00",
+            "patient_id": "PT_SR10_BdVRRejected",
+            "molecular_id": "PT_SR10_BdVRRejected_BD_MOI1",
+            "study_id": "APEC1621",
+            "type": "BLOOD_DNA",
+            "carrier": "Federal Express",
+            "tracking_id": "7956 4568 1235",
+            "destination": "MDA",
+            "dna_volume_ul": "10.0",
+            "dna_concentration_ng_per_ul": "25.0",
+            "cdna_volume_ul": "10.0"
+        }], 200)
+
+    )
+    @unpack
+    @patch('resources.aliquot.PatientEcosystemConnector.verify_molecular_id')
+    @patch('resources.aliquot.SampleControlAccessor')
+    def test_get(self, item, molecular_id, verify_pt_return, expected_results, mock_aliquot_class,
+                 mock_verify_pt_function):
+        instance = mock_aliquot_class.return_value
+        instance.get_item.return_value = item
+        mock_verify_pt_function.return_value = verify_pt_return
+        return_value = self.app.get('/api/v1/aliquot/' + molecular_id)
+        print "==============" + str(return_value.status_code)
+        assert return_value.status_code == expected_results
+        print return_value.data
+        if expected_results == 200:
+            assert len(return_value.data) > 0
+            if len(verify_pt_return) > 0:
+                assert (json.loads(return_value.data))['patient_id'] == "PT_SR10_BdVRRejected"
+            else:
+                assert (json.loads(return_value.data))['site'] == "mocha"
+        else:
+            assert return_value.data.find(molecular_id + " was not found.")
 
     @data(
         ('SC_5AMCC',
