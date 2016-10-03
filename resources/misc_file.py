da2140fb62dab9ddec1d549c5b7eaeef5b9a41b4
wosman <waleed.a.osman@gmail.com>
2016-10-03 16:33:19 -0400

    Altered name input format.

--- resources/misc_file.py
+++ resources/misc_file.py
@@ -4,4 +4,4 @@ from resources.generic_file import GenericFile
 class MiscFile(GenericFile):
 
     def get(self, molecular_id, file_name):
-        return self.get_file_url(molecular_id, str(file_name))
+        return self.get_file_url(molecular_id, str(file_name)+ "_name")
