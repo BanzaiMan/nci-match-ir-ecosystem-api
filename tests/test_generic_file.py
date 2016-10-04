566810b1b67d26ae54989d7dce4245e54a7d2b76
wosman <waleed.a.osman@gmail.com>
2016-10-04 12:31:35 -0400

    Updating generic file unit test

--- tests/test_generic_file.py
+++ tests/test_generic_file.py
@@ -15,7 +15,7 @@ class TestGenericFile(TestCase):
     @data(
         ('SC_SA1CB', 'qc', "u's3_download_file_url': u'https://pedmatch-dev.s3.amazonaws.com/IR_WAO85/"
                            "SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf?",
-         {u'ion_reporter_id': u'IR_WAO85', u'analysis_id': u'SC_SA1CB_SC_SA1CB_a888_v1', u'control_type': u'no_template', u'dna_bam_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_DNA_v1.bam', u'site': u'mocha', u'qc_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf', u'dna_bai_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_DNA_v1.bai', u'cdna_bam_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_RNA_v1.bam', u'vcf_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.vcf', u'cdna_bai_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_RNA_v1.bai', u'date_molecular_id_created': u'2016-09-28 19:56:19.777', u'molecular_id': u'SC_SA1CB'} ,200),
+         {u'ion_reporter_id': u'IR_WAO85', u'analysis_id': u'SC_SA1CB_SC_SA1CB_a888_v1', u'control_type': u'no_template', u'dna_bam_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_DNA_v1.bam', u'site': u'mocha', u'qc_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf', u'dna_bai_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_DNA_v1.bai', u'cdna_bam_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_RNA_v1.bam', u'vcf_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1.vcf', u'cdna_bai_name': u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_RNA_v1.bai', u'date_molecular_id_created': u'2016-09-28 19:56:19.777', u'molecular_id': u'SC_SA1CB'} ,u'https://pedmatch-dev.s3.amazonaws.com/IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf?AWSAccessKeyId=AKIAJXJOWSYB5ULNEKCA&Expires=1475599059&Signature=YiKHi4QNorckhD4ipGX8zXY94RE%3D', 200),
         # ('SC_SA1CB', 'dna_bam', "u's3_download_file_url': u'https://pedmatch-dev.s3.amazonaws.com/IR_WAO85/"
         #                    "SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf?", 200),
         # ('SC_SA1CB', 'cdna_bam', "u's3_download_file_url': u'https://pedmatch-dev.s3.amazonaws.com/IR_WAO85/"
@@ -28,10 +28,10 @@ class TestGenericFile(TestCase):
     )
     @unpack
     @patch('accessors.s3_accessor.S3Accessor')
-    def test_get_file_url(self, molecular_id, file_type, return_message, s3_item, returned_stat_code, mock_s3_accessor, mock_sc_accessor):
+    def test_get_file_url(self, molecular_id, file_type, return_message, s3_item, sc_item, returned_stat_code, mock_s3_accessor, mock_sc_accessor):
         mock_s3_accessor.return_value = s3_item
         sc_instance = mock_sc_accessor.return_value
-        sc_instance.get_item.return_value = True
+        sc_instance.get_item.return_value = sc_item
         return_value = self.app.get('/api/v1/files/' + molecular_id + '/' + file_type)
         print return_value.status_code
         print return_message
