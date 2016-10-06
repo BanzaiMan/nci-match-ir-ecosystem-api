1ce86bf59b4e477c98b33a0c7bc6d905c52a8dca
wosman <waleed.a.osman@gmail.com>
2016-10-06 08:58:49 -0400

    Modified to return status code if not 200.

--- common/patient_ecosystem_connector.py
+++ common/patient_ecosystem_connector.py
@@ -3,25 +3,28 @@ import __builtin__
 
 
 class PatientEcosystemConnector(object):
+    """This class is for connecting with the patient ecosystem in order to verify if the molecular id exists in
+    the patient ecosystem database"""
 
     @staticmethod
     def verify_molecular_id(molecular_id):
+        pt_eco_route = '/api/v1/shipments/'
+        url = (__builtin__.environment_config[__builtin__.environment]['patient_endpoint'] + pt_eco_route)
 
-        url = (__builtin__.environment_config[__builtin__.environment]['patient_endpoint'] + "/api/v1/shipments/")
         try:
-            json_data = PatientEcosystemConnector.open_url(url, molecular_id)
+            request_data = PatientEcosystemConnector.open_url(url, molecular_id)
         except Exception as e:
             raise Exception("Failed to connect to patient ecosystem, because : " + e.message)
 
-        if len(json_data) > 0:
-            return json_data
+        if request_data.status_code == 200:
+            return request_data.json()
         else:
-            raise Exception("Failed to connect to patient ecosystem, because : ")
+            return request_data.status_code
 
     @staticmethod
     def open_url(url, molecular_id):
 
         lookup_url = (url + molecular_id)
-        json_data = requests.get(lookup_url)
+        request_data = requests.get(lookup_url)
 
-        return json_data.json()
+        return request_data
