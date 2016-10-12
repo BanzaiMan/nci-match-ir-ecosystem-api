9d6c790b8392ce590433c7b3c0c12c68712b3767
wosman <waleed.a.osman@gmail.com>
2016-10-12 10:11:43 -0400

    Added error logging.

--- common/patient_ecosystem_connector.py
+++ common/patient_ecosystem_connector.py
@@ -11,16 +11,13 @@ class PatientEcosystemConnector(object):
         self.logger = logging.getLogger(__name__)
 
     def verify_molecular_id(self, molecular_id):
-        # TODO: Waleed.. this is probably best if it was set to debug as we don't necessarily need to see this all the time.
-        self.logger.info("Checking if molecular id: " + str(molecular_id) + " is in patient ecosystem")
-
+        self.logger.debug("Checking if molecular id: " + str(molecular_id) + " is in patient ecosystem")
         url = (__builtin__.environment_config[__builtin__.environment]['patient_endpoint']
                + __builtin__.environment_config[__builtin__.environment]['shipments_path'])
         try:
             request_data = requests.get(url + molecular_id).json()
         except Exception as e:
-            # TODO: Waleed.. this is an error  not "info" need to use error
-            self.logger.info("Unable to connect to patient ecosystem: " + str(e))
+            self.logger.error("Unable to connect to patient ecosystem: " + str(e))
             raise
         else:
             return request_data
