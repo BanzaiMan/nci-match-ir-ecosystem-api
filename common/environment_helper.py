6cdcb178ba14d85ddbe4e732c38c50bfb643c701
wosman <waleed.a.osman@gmail.com>
2016-10-14 14:22:33 -0400

    Simplified code.

--- common/environment_helper.py
+++ common/environment_helper.py
@@ -11,32 +11,19 @@ class EnvironmentHelper(object):
         __builtin__.slack_token = None
         try:
             __builtin__.environment = os.environ['ENVIRONMENT']
-        # TODO: I will simplify this code after the environment variables are set up on TRAVIS.
+            __builtin__.slack_token = os.environ['SLACK_TOKEN']
+
         except KeyError as e:
-            logger_function("Must configure ENVIRONMENT variable in your environment in order for application to start")
+            logger_function("Must configure ENVIRONMENT variable and SLACK_TOKEN variablein your environment in order "
+                            "for application to start")
             logger_function(e.message)
             exit()
         else:
             logger_function("Environment set to: " + __builtin__.environment)
-
-
-            # Use the environment variable from above to read yaml config file and set
-            # global variable to the loaded file so tier specific information may be retrieved
-            # by the various modules as needed.
-            with open(os.path.abspath("config/environment.yml"), 'r') as yaml_file:
-                __builtin__.environment_config = yaml.load(yaml_file)
-
-        try:
-            __builtin__.slack_token = os.environ['SLACK_TOKEN']
-        except KeyError as e:
-            logger_function("Must configure SLACK_TOKEN variable in your environment in order for application to start")
-            logger_function(e.message)
-        else:
             logger_function("Slack token set to: " + __builtin__.slack_token)
 
             # Use the environment variable from above to read yaml config file and set
             # global variable to the loaded file so tier specific information may be retrieved
             # by the various modules as needed.
-
             with open(os.path.abspath("config/environment.yml"), 'r') as yaml_file:
                 __builtin__.environment_config = yaml.load(yaml_file)
