d52a822ae1c151f1b795b88b0ec54ef34c7c3b20
bcoffe <brent.coffey@nih.gov>
2016-10-21 07:23:37 -0400

    Making sure variables only get loaded once when application starts.

--- common/environment_helper.py
+++ common/environment_helper.py
@@ -7,25 +7,32 @@ class EnvironmentHelper(object):
 
     @staticmethod
     def set_environment(logger):
-        __builtin__.environment = None
-        __builtin__.slack_token = None
+        logger.info("Checking if Environment and Slack Token variable loaded")
         try:
-            __builtin__.environment = os.environ['ENVIRONMENT']
-            __builtin__.slack_token = os.environ['SLACK_TOKEN']
+            __builtin__.environment
+            __builtin__.slack_token
+        except Exception as e:
+            logger.info("Environment and Slack Token variable not yet loaded, now attempting to load")
+            try:
+                __builtin__.environment = os.environ['ENVIRONMENT']
+                __builtin__.slack_token = os.environ['SLACK_TOKEN']
 
-        except KeyError as e:
-            # TODO: Change to logger.error
-            logger.error("Must configure ENVIRONMENT variable and SLACK_TOKEN variable in your"
-                                  "environment in order "
-                            "for application to start")
-            logger.error(e.message)
-            exit()
+            except KeyError as e:
+                # TODO: Change to logger.error
+                logger.error("Must configure ENVIRONMENT variable and SLACK_TOKEN variable in your"
+                                      "environment in order "
+                                "for application to start")
+                logger.error(e.message)
+                exit()
+            else:
+                logger.info("Environment set to: " + __builtin__.environment)
+                logger.info("Slack token set to: " + __builtin__.slack_token)
+
+                # Use the environment variable from above to read yaml config file and set
+                # global variable to the loaded file so tier specific information may be retrieved
+                # by the various modules as needed.
+                with open(os.path.abspath("config/environment.yml"), 'r') as yaml_file:
+                    __builtin__.environment_config = yaml.load(yaml_file)
         else:
-            logger.info("Environment set to: " + __builtin__.environment)
-            logger.info("Slack token set to: " + __builtin__.slack_token)
+            logger.info("Environment and Slack Token variable already loaded. Skipping load.")
 
-            # Use the environment variable from above to read yaml config file and set
-            # global variable to the loaded file so tier specific information may be retrieved
-            # by the various modules as needed.
-            with open(os.path.abspath("config/environment.yml"), 'r') as yaml_file:
-                __builtin__.environment_config = yaml.load(yaml_file)
