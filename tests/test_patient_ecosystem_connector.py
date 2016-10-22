7c555655559bf099aea75eb3010dc4a056b03168
yuq4 <qing.yu2@nih.gov>
2016-10-21 22:03:50 -0400

    modify to adapt to PatientEcosystemConnector return value format

--- tests/test_patient_ecosystem_connector.py
+++ tests/test_patient_ecosystem_connector.py
@@ -6,9 +6,6 @@ from mock import patch
 import mock
 import __builtin__
 import app
-import requests
-from requests import RequestException
-
 
 
 @ddt
@@ -19,9 +16,9 @@ class TestPatientEcosystemConnector(TestCase):
         self.patient_ecosystem_connector = PatientEcosystemConnector()
 
     @data(
-        ('SC_SA1CB', '404: Not Found'),
-        ('', '404: Not Found'),
-        ('PT_VU16_BdVRUploaded_BD_MOI1', {"uuid":"98fb0d64-27af-4237-8fbe-22abc0ad2bb8",
+        ('SC_SA1CB', 404, '404: Not Found'),
+        ('', 404, '404: Not Found'),
+        ('PT_VU16_BdVRUploaded_BD_MOI1', 200, {"uuid":"98fb0d64-27af-4237-8fbe-22abc0ad2bb8",
                                           "shipped_date":"2016-05-01T19:42:13+00:00",
                                           "patient_id":"PT_VU16_BdVRUploaded",
                                           "molecular_id":"PT_VU16_BdVRUploaded_BD_MOI1",
@@ -31,7 +28,7 @@ class TestPatientEcosystemConnector(TestCase):
     )
     @unpack
     @patch('common.patient_ecosystem_connector.requests.get')
-    def test_verify_molecular_id(self, molecular_id, expected_result, mock_get):
+    def test_verify_molecular_id(self, molecular_id, expected_statuscode, expected_result, mock_get):
         """
                 Test getting a 200 OK response and 404 from the open_url method of PatientEcosystemConnector.
                 """
@@ -39,8 +36,8 @@ class TestPatientEcosystemConnector(TestCase):
         # behaviours
 
         mock_response = mock.Mock()
-
         mock_response.json.return_value = expected_result
+        mock_response.status_code = expected_statuscode
 
         # Assign our mock response as the result of our patched function
         mock_get.return_value = mock_response
@@ -48,27 +45,28 @@ class TestPatientEcosystemConnector(TestCase):
         url = (__builtin__.environment_config[__builtin__.environment]['patient_endpoint']
                + __builtin__.environment_config[__builtin__.environment]['shipments_path'] + molecular_id)
 
-        response_dict = self.patient_ecosystem_connector.verify_molecular_id(molecular_id=molecular_id)
+        (response_status, response_data) = self.patient_ecosystem_connector.verify_molecular_id(molecular_id=molecular_id)
 
         # Check that our function made the expected internal calls
         mock_get.assert_called_once_with(url)
         print mock_response.json.call_count
         self.assertEqual(1, mock_response.json.call_count)
 
-        print response_dict
+        print response_status
+        print response_data
+        self.assertEqual(response_status, expected_statuscode)
+        self.assertEqual(response_data, expected_result)
 
-        print mock_response.json.call_count
-        # If we want, we can check the contents of the response
-        self.assertEqual(response_dict, expected_result)
 
+    @data('PT_VU16_BdVRUploaded_BD_MOI1')
     @patch('common.patient_ecosystem_connector.requests')
-    def test_get_exception(self, mock_class):
+    def test_get_exception(self, molecular_id, mock_class):
         # Testing exception
 
-        # instance = mock_class.return_value
+        #instance = mock_class.return_value
         mock_class.get.side_effect = Exception('testing throwing exception')
         try:
-            return_value = PatientEcosystemConnector().verify_molecular_id('PT_VU16_BdVRUploaded_BD_MOI1')
+            (response_status, response_data) = PatientEcosystemConnector().verify_molecular_id(molecular_id)
         except Exception as e:
             assert "testing throwing exception" in e.message
         else:
@@ -76,6 +74,6 @@ class TestPatientEcosystemConnector(TestCase):
 
 
 
-
-    if __name__ == "__main__":
-        unittest.main()
+    #
+    # if __name__ == "__main__":
+    #     unittest.main()
