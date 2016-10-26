c22974a2921d010c1f98558acdee04af25131bd2
wosman <waleed.a.osman@gmail.com>
2016-10-26 16:42:54 -0400

    Temporarily reverting changes,

--- resource_helpers/abort_logger.py
+++ resource_helpers/abort_logger.py
@@ -5,13 +5,18 @@ import os
 from flask_restful import abort
 from common.ped_match_bot import PedMatchBot
 
-queue_name = os.environ['IR_QUEUE_NAME']
-slack_channel_id = (__builtin__.environment_config[__builtin__.environment]['slack_channel_id'])
 
 class AbortLogger(object):
 
     @staticmethod
     def log_and_abort(error_code, logger_level_function, message):
+        slack_channel_id = (__builtin__.environment_config[__builtin__.environment]['slack_channel_id'])
+        # TODO: Waleed once you have read the queue name whne the application starts you don't need to continue to read it. Just set a variable once and use it from then on.
+        # this will keep you from having redudant code.
+        if os.environ.get('IR_QUEUE_NAME'):
+            queue_name = os.environ.get('IR_QUEUE_NAME')
+        else:
+            queue_name = (__builtin__.environment_config[__builtin__.environment]['ir_queue_name'])
         try:
             calling_function = inspect.stack()[1][3]
         except Exception as e:
