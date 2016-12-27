bf5a78e817a07dd615e51246360c2948a2a9b127
wosman <waleed.a.osman@gmail.com>
2016-12-27 14:36:30 -0500

    Mocking s3_resource.

--- tests/test_s3_authenticatio_policy.py
+++ tests/test_s3_authenticatio_policy.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from mock import patch
 patch('resources.auth0_resource.requires_auth', lambda x: x).start()
+from resources.s3_authentication_policy import s3_resource
 from ddt import ddt, data, unpack
 import app
 
@@ -23,12 +24,20 @@ class TestS3AuthenticationPolicy(TestCase):
 
     )
     @unpack
-    def test_get(self, ion_reporter_id, molecular_id, analysis_id, file_name, list, status_code):
+    # @patch('resources.s3_authentication_policy.s3_resource')
+    @patch('resources.s3_authentication_policy.bucket2')
+    def test_get(self, ion_reporter_id, molecular_id, analysis_id, file_name, list, status_code, mock_resource):
+    # def test_get(self, ion_reporter_id, molecular_id, analysis_id, file_name, list, status_code):
+        # s3_instance = mock_s3_resource.return_value
+        # s3_instance.ObjectSummary.return_value = True
+
+        s3_instance = mock_resource.objects.filter
+        s3_instance.return_value = [s3_resource.ObjectSummary(bucket_name='pedmatch-dev', key=u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf')]
 
         return_value = self.app.get('/api/v1/sample_controls/files/' + ion_reporter_id + '/' + molecular_id+ '/' + analysis_id+ '/' + file_name)
 
         # print return_value
-        # print return_value.status_code
+        print return_value.status_code
         # print return_value.data
         if 'policy' in return_value.data:
             for i in list:
@@ -36,7 +45,7 @@ class TestS3AuthenticationPolicy(TestCase):
         else:
             assert return_value.status_code == status_code
 
-    # TODO: add 500
+    # # TODO: add 500
     @data(
         ('IR_WAO85','SC_SA1CB', 'SC_SA1CB_SC_SA1CB_a888_v1', 'SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf', ('fields', 'policy', 'AWSAccessKeyId', 'signature'), 409),
         ('', 'SC_SA1CB', 'SC_SA1CB_SC_SA1CB_a888_v1', 'SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf', ('fields', 'policy', 'AWSAccessKeyId', 'signature'), 404),
@@ -49,15 +58,20 @@ class TestS3AuthenticationPolicy(TestCase):
 
     )
     @unpack
+    @patch('resources.s3_authentication_policy.bucket2')
     @patch('resources.s3_authentication_policy.s3Client')
-    def test_get_exception(self, ion_reporter_id, molecular_id, analysis_id, file_name, list, status_code, s3_mock):
+    def test_get_exception(self, ion_reporter_id, molecular_id, analysis_id, file_name, list, status_code, s3_mock, mock_resource):
+        s3_instance2 = mock_resource.objects.filter
+        s3_instance2.return_value = [s3_resource.ObjectSummary(bucket_name='pedmatch-dev',
+                                                              key=u'IR_WAO85/SC_SA1CB/SC_SA1CB_SC_SA1CB_a888_v1/SC_SA1CB_SC_SA1CB_analysis888_v1_QC.pdf')]
+
         s3_instance = s3_mock.return_value
         s3_instance.generate_presigned_post.return_value = False
 
         return_value = self.app.get('/api/v1/sample_controls/files/' + ion_reporter_id + '/' + molecular_id+ '/' + analysis_id+ '/' + file_name)
 
         # print return_value
-        # print return_value.status_code
+        print return_value.status_code
         # print return_value.data
         if 'policy' in return_value.data:
             for i in list:
