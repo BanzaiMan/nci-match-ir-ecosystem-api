0ddbb50c8d45b632a8f51414fbda31d7a64d2178
wosman <waleed.a.osman@gmail.com>
2016-12-28 11:31:47 -0500

    Modifying error messaging.

---
+++ scripts/database/get_id_token.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# *******************************************************************
+# * Script to run curl commands for retrieving id_token.                       *
+# *******************************************************************
+
+
+id_token= curl -H "Content-Type: application/json" -X POST -d '{"client_id":"'"$AUTH0_CLIENT_ID"'","username":"'"$AUTH0_USERNAME"'","password":"'"$AUTH0_PASSWORD"'","grant_type":"password","scope":"openid","connection":"'"$AUTH0_DATABASE"'"}' https://ncimatch.auth0.com/oauth/ro
