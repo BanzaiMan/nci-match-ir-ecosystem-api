fa564a5f61f567d7c7be913b61901ac0b7f08a1e
wosman <waleed.a.osman@gmail.com>
2017-01-03 11:34:17 -0500

    Adding check for unit tests to authorization function.

--- resources/auth0_resource.py
+++ resources/auth0_resource.py
@@ -19,37 +19,39 @@ def authenticate(error):
 
 
 def requires_auth(function):
-  if os.environ['ENVIRONMENT'] == 'development' or os.environ['ENVIRONMENT']== 'test':
-    return function
-  @wraps(function)
-  def decorated(*args, **kwargs):
-    auth = request.headers.get('Authorization', None)
-    if not auth:
-      return authenticate({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'})
-
-    parts = auth.split()
-
-    if parts[0].lower() != 'bearer':
-      return {'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}
-    elif len(parts) == 1:
-      return {'code': 'invalid_header', 'description': 'Token not found'}
-    elif len(parts) > 2:
-      return {'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}
-    token = parts[1]
-    auth0_client_secret = base64.b64decode(os.environ['AUTH0_CLIENT_SECRET'].replace("_", "/").replace("-", "+"))
-    auth0_client_id = os.environ['AUTH0_CLIENT_ID']
     try:
-        payload = jwt.decode(token, auth0_client_secret, audience=auth0_client_id)
-    except jwt.ExpiredSignature:
-        return authenticate({'code': 'token_expired', 'description': 'token is expired'})
-    except jwt.InvalidAudienceError:
-        return authenticate({'code': 'invalid_audience', 'description': 'incorrect audience, expected: ' + auth0_client_id })
-    except jwt.DecodeError:
-        return authenticate({'code': 'token_invalid_signature', 'description': 'token signature is invalid'})
+        os.environ['UNITTEST']
+        return function
+    except Exception:
+      @wraps(function)
+      def decorated(*args, **kwargs):
+        auth = request.headers.get('Authorization', None)
+        if not auth:
+          return authenticate({'code': 'authorization_header_missing', 'description': 'Authorization header is expected'})
 
-    _request_ctx_stack.top.current_user = user = payload
-    return function(*args, **kwargs)
-  return decorated
+        parts = auth.split()
+
+        if parts[0].lower() != 'bearer':
+          return {'code': 'invalid_header', 'description': 'Authorization header must start with Bearer'}
+        elif len(parts) == 1:
+          return {'code': 'invalid_header', 'description': 'Token not found'}
+        elif len(parts) > 2:
+          return {'code': 'invalid_header', 'description': 'Authorization header must be Bearer + \s + token'}
+        token = parts[1]
+        auth0_client_secret = base64.b64decode(os.environ['AUTH0_CLIENT_SECRET'].replace("_", "/").replace("-", "+"))
+        auth0_client_id = os.environ['AUTH0_CLIENT_ID']
+        try:
+            payload = jwt.decode(token, auth0_client_secret, audience=auth0_client_id)
+        except jwt.ExpiredSignature:
+            return authenticate({'code': 'token_expired', 'description': 'token is expired'})
+        except jwt.InvalidAudienceError:
+            return authenticate({'code': 'invalid_audience', 'description': 'incorrect audience, expected: ' + auth0_client_id })
+        except jwt.DecodeError:
+            return authenticate({'code': 'token_invalid_signature', 'description': 'token signature is invalid'})
+
+        _request_ctx_stack.top.current_user = user = payload
+        return function(*args, **kwargs)
+      return decorated
 
 
 class Auth0Resource(Resource):
